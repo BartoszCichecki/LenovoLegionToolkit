958e52757358857b80c9940d4ea04e5621bb82e7
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-10-19 23:52:02 +0200

    Added compatibility for 16ACHg6 and 16ITHg6.

---
+++ Lib/Utils/Compatibility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LenovoLegionToolkit.Lib.Utils
+{
+    public static class Compatibility
+    {
+        private static readonly string _allowedVendor = "LENOVO";
+        private static readonly string[] _allowedModels = new[]
+        {
+            "16ACHg6", //Legion 7 - AMD, nVidia
+            "16ITHg6", //Lenovo 7 - Intel, nVidia
+            "16ACH6H", //Legion 5 Pro - AMD, nVidia
+        };
+
+        public static bool IsCompatible(MachineInformation machineInformation)
+        {
+            if (machineInformation.Vendor.Equals(_allowedVendor, StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            foreach (string allowedModel in _allowedModels)
+            {
+                if (!machineInformation.Model.Contains(allowedModel, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Lib/Utils/Windows.cs
+++ Lib/Utils/Windows.cs
@@ -3,18 +3,31 @@ using System.Management;
 
 namespace LenovoLegionToolkit.Lib.Utils
 {
+    public struct MachineInformation
+    {
+        public string Vendor;
+        public string Model;
+    }
     public static class Windows
     {
         public static void Restart() => ExecuteProcess("shutdown", "-r -t 0");
 
         public static void SetPowerPlan(string guid) => ExecuteProcess("powercfg", $"-setactive {guid}");
 
-        public static string GetMachineVersion()
+        public static MachineInformation GetMachineInformation()
         {
             var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_ComputerSystemProduct");
             foreach (var queryObj in searcher.Get())
-                return queryObj["Version"].ToString();
-            return null;
+            {
+                var vendor = queryObj["Vendor"].ToString();
+                var model = queryObj["Version"].ToString();
+                return new MachineInformation
+                {
+                    Vendor = vendor,
+                    Model = model,
+                };
+            }
+            return default;
         }
 
         private static bool ExecuteProcess(string file, string arguments)
