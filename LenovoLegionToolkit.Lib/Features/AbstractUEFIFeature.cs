547629995f1c093cda7fad995cf43a301eadaa52
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-08 20:09:40 +0100

    Added trace to abstract features

--- LenovoLegionToolkit.Lib/Features/AbstractUEFIFeature.cs
+++ LenovoLegionToolkit.Lib/Features/AbstractUEFIFeature.cs
@@ -22,23 +22,38 @@ namespace LenovoLegionToolkit.Lib.Features
 
         protected S ReadFromUefi<S>(S structure) where S : struct
         {
+            Log.Instance.Trace($"Reading from UEFI... [feature={GetType().Name}]");
+
             if (!IsUefiMode())
-                throw new InvalidOperationException("UEFI mode is not enabled.");
+            {
+                Log.Instance.Trace($"UEFI mode is not enabled. [feature={GetType().Name}]");
+                throw new InvalidOperationException("UEFI mode is not enabled");
+            }
 
             var hGlobal = Marshal.AllocHGlobal(Marshal.SizeOf<S>());
 
             try
             {
                 if (!SetPrivilege(true))
-                    throw new InvalidOperationException($"Cannot set privilages UEFI.");
+                {
+                    Log.Instance.Trace($"Cannot set UEFI privilages [feature={GetType().Name}]");
+                    throw new InvalidOperationException($"Cannot set privilages UEFI");
+                }
 
                 var ptr = hGlobal;
 
                 Marshal.StructureToPtr(structure, ptr, false);
                 if (Native.GetFirmwareEnvironmentVariableExW(_scopeName, _guid, hGlobal, Marshal.SizeOf<S>(), IntPtr.Zero) != 0)
-                    return (S)Marshal.PtrToStructure(hGlobal, typeof(S));
+                {
+                    var result = (S)Marshal.PtrToStructure(hGlobal, typeof(S));
+                    Log.Instance.Trace($"Read from UEFI [feature={GetType().Name}]");
+                    return result;
+                }
                 else
-                    throw new InvalidOperationException($"Cannot read variable {_scopeName} from UEFI.");
+                {
+                    Log.Instance.Trace($"Cannot read vairable {_scopeName} from UEFI [feature={GetType().Name}]");
+                    throw new InvalidOperationException($"Cannot read variable {_scopeName} from UEFI");
+                }
             }
             finally
             {
@@ -50,19 +65,28 @@ namespace LenovoLegionToolkit.Lib.Features
         protected void WriteToUefi<S>(S structure) where S : struct
         {
             if (!IsUefiMode())
+            {
+                Log.Instance.Trace($"UEFI mode is not enabled. [feature={GetType().Name}]");
                 throw new InvalidOperationException("UEFI mode is not enabled.");
+            }
 
             var hGlobal = Marshal.AllocHGlobal(Marshal.SizeOf<S>());
 
             try
             {
                 if (!SetPrivilege(true))
-                    throw new InvalidOperationException($"Cannot set privilages UEFI.");
+                {
+                    Log.Instance.Trace($"Cannot set UEFI privilages [feature={GetType().Name}]");
+                    throw new InvalidOperationException($"Cannot set privilages UEFI");
+                }
 
                 var ptr = hGlobal;
                 Marshal.StructureToPtr(structure, ptr, false);
                 if (Native.SetFirmwareEnvironmentVariableExW(_scopeName, _guid, hGlobal, Marshal.SizeOf<S>(), _scopeAttribute) != 1)
-                    throw new InvalidOperationException($"Cannot write variable {_scopeName} to UEFI.");
+                {
+                    Log.Instance.Trace($"Cannot write vairable {_scopeName} to UEFI [feature={GetType().Name}]");
+                    throw new InvalidOperationException($"Cannot write variable {_scopeName} to UEFI");
+                }
             }
             finally
             {
@@ -71,36 +95,52 @@ namespace LenovoLegionToolkit.Lib.Features
             }
         }
 
-        private static bool IsUefiMode()
+        private bool IsUefiMode()
         {
             var firmwareType = FirmwareType.Unknown;
             if (Native.GetFirmwareType(ref firmwareType))
-                return firmwareType == FirmwareType.Uefi;
+            {
+                var result = firmwareType == FirmwareType.Uefi;
+                Log.Instance.Trace($"Firmware type is {result} [feature={GetType().Name}]");
+                return result;
+            }
 
+            Log.Instance.Trace($"Could not get firmware type [feature={GetType().Name}]");
             return false;
         }
 
-        private static bool SetPrivilege(bool enable)
+        private bool SetPrivilege(bool enable)
         {
             try
             {
                 var zero = IntPtr.Zero;
                 if (!Native.OpenProcessToken(Native.GetCurrentProcess(), 40U, ref zero))
                 {
+                    Log.Instance.Trace($"Could not open process token [feature={GetType().Name}]");
                     return false;
                 }
+
                 TokenPrivelege newState;
                 newState.Count = 1;
                 newState.Luid = 0L;
                 newState.Attr = enable ? 2 : 0;
                 if (!Native.LookupPrivilegeValue(null, "SeSystemEnvironmentPrivilege", ref newState.Luid))
+                {
+                    Log.Instance.Trace($"Could not look up privilege value [feature={GetType().Name}]");
                     return false;
+                }
+
                 if (!Native.AdjustTokenPrivileges(zero, false, ref newState, 0, IntPtr.Zero, IntPtr.Zero))
+                {
+                    Log.Instance.Trace($"Could not adjust token privileges [feature={GetType().Name}]");
                     return false;
+                }
+
                 return true;
             }
-            catch
+            catch (Exception ex)
             {
+                Log.Instance.Trace($"Exception: {ex} [feature={GetType().Name}]");
                 return false;
             }
         }
--- LenovoLegionToolkit.Lib/Features/AbstractWmiFeature.cs
+++ LenovoLegionToolkit.Lib/Features/AbstractWmiFeature.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Management;
+using LenovoLegionToolkit.Lib.Utils;
 
 namespace LenovoLegionToolkit.Lib.Features
 {
@@ -21,19 +22,27 @@ namespace LenovoLegionToolkit.Lib.Features
 
         public virtual T GetState()
         {
+            Log.Instance.Trace($"Getting state... [feature={GetType().Name}]");
             if (!IsSupported())
+            {
+                Log.Instance.Trace($"Feature {_methodNameSuffix} is not supported [feature={GetType().Name}]");
                 throw new NotSupportedException($"Feature {_methodNameSuffix} is not supported.");
+            }
 
-            return FromInternal(ExecuteGamezone("Get" + _methodNameSuffix, "Data"));
+            var result = FromInternal(ExecuteGamezone("Get" + _methodNameSuffix, "Data"));
+            Log.Instance.Trace($"State is {result} [feature={GetType().Name}]");
+            return result;
         }
 
         public virtual void SetState(T state)
         {
+            Log.Instance.Trace($"Setting state to {state}... [feature={GetType().Name}]");
             ExecuteGamezone("Set" + _methodNameSuffix, "Data",
                 new Dictionary<string, string>
                 {
                     {"Data", ToInternal(state).ToString()}
                 });
+            Log.Instance.Trace($"Set state to {state} [feature={GetType().Name}]");
         }
 
         private bool IsSupported()
@@ -49,20 +58,25 @@ namespace LenovoLegionToolkit.Lib.Features
 
         private T FromInternal(int state) => (T)(object)(state - _offset);
 
-        private static int ExecuteGamezone(string methodName, string resultPropertyName, Dictionary<string, string> methodParams = null)
+        private int ExecuteGamezone(string methodName, string resultPropertyName, Dictionary<string, string> methodParams = null)
         {
             return Execute("SELECT * FROM LENOVO_GAMEZONE_DATA", methodName, resultPropertyName, methodParams);
         }
 
-        private static int Execute(string queryString, string methodName, string resultPropertyName,
+        private int Execute(string queryString,
+            string methodName,
+            string resultPropertyName,
             Dictionary<string, string> methodParams = null)
         {
-            var scope = new ManagementScope("ROOT\\WMI");
-            scope.Connect();
-            var objectQuery = new ObjectQuery(queryString);
-            using var enumerator = new ManagementObjectSearcher(scope, objectQuery).Get().GetEnumerator();
+            Log.Instance.Trace($"Executing WMI query... [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
+
+            using var enumerator = new ManagementObjectSearcher("ROOT\\WMI", queryString).Get().GetEnumerator();
             if (!enumerator.MoveNext())
+            {
+                Log.Instance.Trace($"No results in query [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
                 throw new InvalidOperationException("No results in query");
+            }
+
             var mo = (ManagementObject)enumerator.Current;
             var methodParamsObject = mo.GetMethodParameters(methodName);
             if (methodParams != null)
@@ -70,7 +84,11 @@ namespace LenovoLegionToolkit.Lib.Features
                     methodParamsObject[pair.Key] = pair.Value;
 
             var result = mo.InvokeMethod(methodName, methodParamsObject, null)?.Properties[resultPropertyName].Value;
-            return Convert.ToInt32(result);
+            var intResult = Convert.ToInt32(result);
+
+            Log.Instance.Trace($"Executed WMI query with result {intResult} [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
+
+            return intResult;
         }
     }
 }
