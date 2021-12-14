b8b41f6ccbf7f464bfc374e33ffca2b3a81903f7
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-14 19:17:06 +0100

    Implemented two test features.

---
+++ LenovoLegionToolkit.WPF/Container.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using LenovoLegionToolkit.Lib.Features;
+using LenovoLegionToolkit.Lib.Listeners;
+
+namespace LenovoLegionToolkit.WPF
+{
+    internal class Container
+    {
+        private static IContainer _container;
+
+        public static void Initialize()
+        {
+            var cb = new ContainerBuilder();
+
+            cb.RegisterType<AlwaysOnUsbFeature>();
+            cb.RegisterType<BatteryFeature>();
+            cb.RegisterType<FlipToStartFeature>();
+            cb.RegisterType<FnLockFeature>();
+            cb.RegisterType<HybridModeFeature>();
+            cb.RegisterType<OverDriveFeature>();
+            cb.RegisterType<PowerModeFeature>()
+                .OnActivated(e => e.Instance.EnsureCorrectPowerPlanIsSet());
+            cb.RegisterType<RefreshRateFeature>();
+            cb.RegisterType<TouchpadLockFeature>();
+
+            cb.RegisterType<PowerModeListener>();
+
+            _container = cb.Build();
+        }
+
+        public static T Resolve<T>() => _container.Resolve<T>();
+    }
+}
--- LenovoLegionToolkit.WPF/Controls/BatteryModeControl.xaml.cs
+++ LenovoLegionToolkit.WPF/Controls/BatteryModeControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using LenovoLegionToolkit.Lib;
+using LenovoLegionToolkit.Lib.Features;
 
 namespace LenovoLegionToolkit.WPF.Controls
 {
@@ -7,9 +11,36 @@ namespace LenovoLegionToolkit.WPF.Controls
     /// </summary>
     public partial class BatteryModeControl : UserControl
     {
+        private readonly BatteryFeature _feature = Container.Resolve<BatteryFeature>();
+
         public BatteryModeControl()
         {
             InitializeComponent();
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            try
+            {
+                foreach (var item in Enum.GetValues(typeof(BatteryState)))
+                    _comboBox.Items.Add(item);
+                _comboBox.SelectedItem = _feature.GetState();
+                Visibility = Visibility.Visible;
+            }
+            catch
+            {
+                _comboBox.Items.Clear();
+                _comboBox.SelectedItem = null;
+                Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var state = (BatteryState)_comboBox.SelectedItem;
+            if (state != _feature.GetState())
+                _feature.SetState(state);
         }
     }
 }
