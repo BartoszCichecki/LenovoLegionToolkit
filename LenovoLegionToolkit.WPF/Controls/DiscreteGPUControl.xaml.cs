e1ed2526f7227b502c37f865e7442e9fd1ecfd42
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-14 21:22:18 +0100

    Moved refreshing into visibility change

--- LenovoLegionToolkit.WPF/Controls/DiscreteGPUControl.xaml.cs
+++ LenovoLegionToolkit.WPF/Controls/DiscreteGPUControl.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace LenovoLegionToolkit.WPF.Controls
+﻿using System.Windows;
+
+namespace LenovoLegionToolkit.WPF.Controls
 {
     public partial class DiscreteGPUControl
     {
         public DiscreteGPUControl()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
         }
     }
 }
