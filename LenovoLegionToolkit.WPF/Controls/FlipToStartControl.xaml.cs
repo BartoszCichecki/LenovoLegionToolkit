fc7245ca973d594c588b534df659f492ac39b1a2
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-14 19:41:02 +0100

    Implemented some features.

--- LenovoLegionToolkit.WPF/Controls/FlipToStartControl.xaml.cs
+++ LenovoLegionToolkit.WPF/Controls/FlipToStartControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using LenovoLegionToolkit.Lib;
+using LenovoLegionToolkit.Lib.Features;
 
 namespace LenovoLegionToolkit.WPF.Controls
 {
@@ -7,9 +10,33 @@ namespace LenovoLegionToolkit.WPF.Controls
     /// </summary>
     public partial class FlipToStartControl : UserControl
     {
+        private readonly FlipToStartFeature _feature = Container.Resolve<FlipToStartFeature>();
+
         public FlipToStartControl()
         {
             InitializeComponent();
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            try
+            {
+                _toggleButton.IsChecked = _feature.GetState() == FlipToStartState.On;
+                Visibility = Visibility.Visible;
+            }
+            catch
+            {
+                _toggleButton.IsChecked = false;
+                Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var state = _toggleButton.IsChecked.Value ? FlipToStartState.On : FlipToStartState.Off;
+            if (state != _feature.GetState())
+                _feature.SetState(state);
         }
     }
 }
