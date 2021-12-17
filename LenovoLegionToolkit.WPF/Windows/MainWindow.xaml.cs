a0d672429f71198b8d48f90c36b97d697567b9e9
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-17 23:22:11 +0100

    Moved fully to main window

--- LenovoLegionToolkit.WPF/Windows/MainWindow.xaml.cs
+++ LenovoLegionToolkit.WPF/Windows/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
+using LenovoLegionToolkit.Lib.Listeners;
+using LenovoLegionToolkit.Lib;
+using LenovoLegionToolkit.Lib.Utils;
 using LenovoLegionToolkit.WPF.Pages;
 using WPFUI.Common;
 
@@ -30,6 +35,64 @@ namespace LenovoLegionToolkit.WPF.Windows
             };
 
             RootNavigation.Navigate("dashboard");
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (Log.Instance.IsTraceEnabled)
+                Log.Instance.Trace($"Window state changed to {WindowState}");
+
+            switch (WindowState)
+            {
+                case WindowState.Minimized:
+                    SendToTray();
+                    break;
+                case WindowState.Normal:
+                    BringToForeground();
+                    break;
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (Settings.Instance.MinimizeOnClose)
+            {
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Minimizing...");
+
+                WindowState = WindowState.Minimized;
+                e.Cancel = true;
+            }
+            else
+            {
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Closing...");
+            }
+        }
+
+        private void NotifyIcon_Open(object sender, RoutedEventArgs e) => BringToForeground();
+
+        private void NotifyIcon_Exit(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        public void BringToForeground()
+        {
+            ShowInTaskbar = true;
+
+            if (WindowState == WindowState.Minimized || Visibility == Visibility.Hidden)
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            }
+
+            Activate();
+            Topmost = true;
+            Topmost = false;
+            Focus();
+        }
+        public void SendToTray()
+        {
+            Hide();
+            ShowInTaskbar = false;
         }
     }
 }
