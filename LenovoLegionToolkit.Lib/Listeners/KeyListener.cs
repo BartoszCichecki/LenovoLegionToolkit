36102f382fc773af4b1e421f0e2060a9cba120e9
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-21 20:20:10 +0100

    Internal screen can also be connected with displayport embeeded connection.

--- LenovoLegionToolkit.Lib/Listeners/KeyListener.cs
+++ LenovoLegionToolkit.Lib/Listeners/KeyListener.cs
@@ -1,8 +1,8 @@
 ﻿namespace LenovoLegionToolkit.Lib.Listeners
 {
-    public class KeyListener: AbstractWMIListener<Key>
+    public class KeyListener : AbstractWMIListener<Key>
     {
-        public KeyListener(): base("LENOVO_UTILITY_EVENT", "PressTypeDataVal", 0) { }
+        public KeyListener() : base("LENOVO_UTILITY_EVENT", "PressTypeDataVal", 0) { }
 
         protected override void OnChanged(Key value)
         {
