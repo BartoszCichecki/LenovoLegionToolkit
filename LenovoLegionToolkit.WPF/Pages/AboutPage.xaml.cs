3d48be1c0924fa3d67d7b38d58369b026261ae81
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-01-01 16:56:00 +0100

    About page

--- LenovoLegionToolkit.WPF/Pages/AboutPage.xaml.cs
+++ LenovoLegionToolkit.WPF/Pages/AboutPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace LenovoLegionToolkit.WPF.Pages
+﻿using System.Reflection;
+
+namespace LenovoLegionToolkit.WPF.Pages
 {
     public partial class AboutPage
     {
         public AboutPage()
         {
             InitializeComponent();
+
+            _version.Text += Assembly.GetEntryAssembly().GetName().Version.ToString(3);
         }
     }
 }
