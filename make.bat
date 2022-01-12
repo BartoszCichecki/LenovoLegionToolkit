ba3f0803b5adfd63ed2406ffdfecc0de67f28c75
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-01-12 15:03:45 +0100

    Fix convenience script

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-dotnet publish LenovoLegionToolkit.WPF -c release -o build -a x64 /p:DebugType=None /p:FileVersion=0.0.1 /p:Version=0.0.1
+dotnet publish LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=0.0.1 /p:Version=0.0.1
