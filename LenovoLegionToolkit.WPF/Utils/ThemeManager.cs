b16896e6030ea1405027eff5938e64339afe2a88
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-01-15 00:26:51 +0100

    Corrected automatic theme switching.

--- LenovoLegionToolkit.WPF/Utils/ThemeManager.cs
+++ LenovoLegionToolkit.WPF/Utils/ThemeManager.cs
@@ -1,44 +1,48 @@
-﻿using System.ComponentModel;
+﻿using System;
 using System.Windows;
 using System.Windows.Media;
 using LenovoLegionToolkit.Lib;
+using LenovoLegionToolkit.Lib.Utils;
 
 namespace LenovoLegionToolkit.WPF.Utils
 {
     public class ThemeManager
     {
+        private const string RegistryHive = "HKEY_CURRENT_USER";
+        private const string RegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+        private const string RegistryKey = "AppsUseLightTheme";
+
+#pragma warning disable IDE0052 // Remove unread private members
+        private readonly IDisposable _themeListener;
+#pragma warning restore IDE0052 // Remove unread private members
+
         public ThemeManager()
         {
-            SystemParameters.StaticPropertyChanged += SystemParameters_StaticPropertyChanged;
+            _themeListener = Registry.Listen(RegistryHive, RegistryPath, RegistryKey, () => Application.Current.Dispatcher.Invoke(() =>
+            {
+                Apply();
+            }));
         }
 
         public void Apply()
         {
-            switch (Settings.Instance.Theme)
+            var theme = Settings.Instance.Theme;
+            var registryValue = Registry.Read(RegistryHive, RegistryPath, RegistryKey, 1);
+
+            var currentTheme = (theme, registryValue) switch
             {
-                case Theme.Dark:
-                    WPFUI.Theme.Manager.Switch(WPFUI.Theme.Style.Dark);
-                    break;
-                case Theme.Light:
-                    WPFUI.Theme.Manager.Switch(WPFUI.Theme.Style.Light);
-                    break;
-                case Theme.System:
-                    WPFUI.Theme.Manager.SetSystemTheme();
-                    break;
-            }
+                (Theme.Light, _) => WPFUI.Theme.Style.Light,
+                (Theme.System, 1) => WPFUI.Theme.Style.Light,
+                _ => WPFUI.Theme.Style.Dark,
+            };
+            WPFUI.Theme.Manager.Switch(currentTheme);
 
             SetColor();
         }
 
-        private void SystemParameters_StaticPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "WindowGlassColor" && Settings.Instance.Theme == Theme.System)
-                Apply();
-        }
-
         private static void SetColor()
         {
-            var accentColor = (Color)ColorConverter.ConvertFromString("#f44336");
+            var accentColor = (Color)ColorConverter.ConvertFromString("#F44336");
 
             Application.Current.Resources["SystemAccentColor"] = accentColor;
             Application.Current.Resources["SystemAccentColorLight2"] = accentColor;
