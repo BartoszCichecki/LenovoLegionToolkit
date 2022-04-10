786a53c01ce1791464c9def944f0da73ae05492e
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-04-10 20:19:09 +0200

    Added compatibility for a couple models

--- LenovoLegionToolkit.Lib/Utils/Compatibility.cs
+++ LenovoLegionToolkit.Lib/Utils/Compatibility.cs
@@ -25,11 +25,14 @@ namespace LenovoLegionToolkit.Lib.Utils
             "16ITHg6", // Legion 7 - Intel, nVidia
             "15IMHg05", // Legion 7 - Intel, nVidia
             "16ACH6", // Legion 5 Pro - AMD, nVidia
+            "R7000P2020H", // Legion 5 Pro - AMD, nVidia (CN)
             "16ITH6", // Legion 5 Pro - Intel, nVidia
+            "16IAH7H", // Legion 5 Pro - Intel, nVidia
             "17ACH6", // Legion 5 - AMD, nVidia
             "15ACH6", // Legion 5 - AMD, nVidia
             "15ARH05", // Legion 5 - AMD, nVidia
             "15IMH05", // Legion 5 - Intel, nVidia
+            "15ITH6H", // Legion 5 - Intel, nVidia
             "17IMH05H", // Legion 5 - Intel, nVidia
             "R7000P2021H", // Legion 5 - AMD, nVidia (CN)
             "15ACH6", // Legion S7 - AMD, nVidia
