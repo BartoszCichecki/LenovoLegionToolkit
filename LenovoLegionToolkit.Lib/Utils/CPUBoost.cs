82852c29259008e11bf7726ecc7d2512ef02f3db
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-05-16 20:53:19 +0200

    Fix crash described in #98

--- LenovoLegionToolkit.Lib/Utils/CPUBoost.cs
+++ LenovoLegionToolkit.Lib/Utils/CPUBoost.cs
@@ -39,7 +39,6 @@ namespace LenovoLegionToolkit.Lib.Utils
 
             EnsureAttributeVisible();
 
-
             if (Log.Instance.IsTraceEnabled)
                 Log.Instance.Trace($"Getting power plans...");
 
