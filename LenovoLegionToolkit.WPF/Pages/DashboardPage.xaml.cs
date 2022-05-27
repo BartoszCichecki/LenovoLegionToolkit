dbaafcd1507c8a4aacfa4a28cccfc478d6a16b67
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-05-28 01:42:31 +0200

    Layout tweaks

--- LenovoLegionToolkit.WPF/Pages/DashboardPage.xaml.cs
+++ LenovoLegionToolkit.WPF/Pages/DashboardPage.xaml.cs
@@ -17,24 +17,20 @@ namespace LenovoLegionToolkit.WPF.Pages
             if (!e.WidthChanged)
                 return;
 
-            if (e.NewSize.Width < 1000)
+            if (e.NewSize.Width < 950)
             {
                 _column0.Width = new GridLength(1, GridUnitType.Star);
                 _column1.Width = new GridLength(0, GridUnitType.Pixel);
                 _column2.Width = new GridLength(0, GridUnitType.Pixel);
 
-                _row1.Height = new GridLength(16, GridUnitType.Pixel);
-
                 Grid.SetColumn(_stackPanelRight, 0);
-                Grid.SetRow(_stackPanelRight, 2);
+                Grid.SetRow(_stackPanelRight, 1);
             }
             else
             {
                 _column0.Width = new GridLength(1, GridUnitType.Star);
                 _column1.Width = new GridLength(16, GridUnitType.Pixel);
                 _column2.Width = new GridLength(1, GridUnitType.Star);
-
-                _row1.Height = new GridLength(0, GridUnitType.Pixel);
 
                 Grid.SetColumn(_stackPanelRight, 2);
                 Grid.SetRow(_stackPanelRight, 0);
