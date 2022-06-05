07f657c3cb5262c8c49e950b3d1c2ee5b4924969
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-05 14:42:09 +0200

    Added power plan listener

---
+++ LenovoLegionToolkit.Lib/Listeners/AbstractEventLogListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace LenovoLegionToolkit.Lib.Listeners
+{
+
+    public abstract class AbstractEventLogListener : IListener<EventArgs>
+    {
+        private readonly EventLogWatcher _watcher;
+
+        public AbstractEventLogListener(string path, string query)
+        {
+            var eventLogQuery = new EventLogQuery(path, PathType.LogName, query);
+            _watcher = new EventLogWatcher(eventLogQuery);
+            _watcher.EventRecordWritten += Watcher_EventRecordWritten;
+            _watcher.Enabled = true;
+        }
+
+        public event EventHandler<EventArgs>? Changed;
+
+        protected abstract void OnChanged();
+
+        private void Watcher_EventRecordWritten(object? sender, EventRecordWrittenEventArgs e)
+        {
+            OnChanged();
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ LenovoLegionToolkit.Lib/Listeners/PowerPlanListener.cs
@@ -0,0 +1,13 @@
+﻿namespace LenovoLegionToolkit.Lib.Listeners
+{
+    public class PowerPlanListener : AbstractEventLogListener
+    {
+        public PowerPlanListener() : base("System", "*[System[Provider[@Name='Microsoft-Windows-UserModePowerService'] and EventID=12]]")
+        {
+        }
+
+        protected override void OnChanged()
+        {
+        }
+    }
+}
