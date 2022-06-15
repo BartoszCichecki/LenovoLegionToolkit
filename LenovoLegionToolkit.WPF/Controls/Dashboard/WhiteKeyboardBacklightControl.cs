16c9ebe8fa73bcf8414d1a81d45d308b5a604f14
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-16 01:12:01 +0200

    Added white keyboard brightness control

---
+++ LenovoLegionToolkit.WPF/Controls/Dashboard/WhiteKeyboardBacklightControl.cs
@@ -0,0 +1,15 @@
+﻿using LenovoLegionToolkit.Lib;
+using WPFUI.Common;
+
+namespace LenovoLegionToolkit.WPF.Controls.Dashboard
+{
+    internal class WhiteKeyboardBacklightControl : AbstractComboBoxCardControl<WhiteKeyboardBacklightStatus>
+    {
+        public WhiteKeyboardBacklightControl()
+        {
+            Icon = SymbolRegular.Keyboard24;
+            Title = "Keyboard Backlight";
+            Subtitle = "Adjust keyboard backlight brightness.\nYou can change brightness using shortcut Fn+Space";
+        }
+    }
+}
