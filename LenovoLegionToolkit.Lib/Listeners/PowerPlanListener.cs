f9cadc1a5b9eeb7a03c36df6774f8515232da1c5
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-15 21:24:48 +0200

    Refactoring

--- LenovoLegionToolkit.Lib/Listeners/PowerPlanListener.cs
+++ LenovoLegionToolkit.Lib/Listeners/PowerPlanListener.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using LenovoLegionToolkit.Lib.Features;
+using LenovoLegionToolkit.Lib.System;
 using LenovoLegionToolkit.Lib.Utils;
 
 namespace LenovoLegionToolkit.Lib.Listeners
