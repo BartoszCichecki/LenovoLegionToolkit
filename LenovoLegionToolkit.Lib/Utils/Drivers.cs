025a4825a65120b944487ce16159646d91a04db9
SmokelessCPU <smokeless@example.com>
2022-06-15 19:42:19 +0200

    Added Hid Driver Pinvoke and Keyboard RGB

--- LenovoLegionToolkit.Lib/Utils/Drivers.cs
+++ LenovoLegionToolkit.Lib/Utils/Drivers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
 
 namespace LenovoLegionToolkit.Lib.Utils
@@ -8,11 +9,12 @@ namespace LenovoLegionToolkit.Lib.Utils
         private static readonly object _locker = new();
 
         private static SafeFileHandle? _energy;
-
+        private static SafeFileHandle? _hid;
         public static SafeFileHandle GetEnergy()
         {
             if (_energy == null)
             {
+                LocateHid(0x048D, 0xC955, 0xFF0F, 33);
                 lock (_locker)
                 {
                     if (_energy == null)
@@ -25,6 +27,102 @@ namespace LenovoLegionToolkit.Lib.Utils
                 }
             }
             return _energy;
+        }
+
+
+        public static Boolean IsRGBKeyboardSupported()
+        {
+            return (LocateHid(0x048D, 0xC955, 0xFF0F, 33) != null);
+        }
+
+        public static SafeFileHandle GetHid()
+        {
+            if (_hid == null)
+            {
+                _hid = LocateHid(0x048D, 0xC955, 0xFF0F, 33);
+                if (_hid == null)
+                    throw new InvalidOperationException("fileHandle is 0");
+
+            }
+            return _hid;
+        }
+
+        public static SafeFileHandle? LocateHid(ushort VendorId, ushort ProductId, ushort ProductIdMask, ushort DescriptorLenght)
+        {
+            SafeFileHandle? hid = null;
+            lock (_locker)
+            {
+                //TODO Find a Better Way to locate the HID
+                Guid HIDClassGuid;
+                Native.HidD_GetHidGuid(out HIDClassGuid);
+                // Todo Define DIGCF_PRESENT | DIGCF_DEVICEINTERFACE
+                IntPtr DeviceInfoSet = Native.SetupDiGetClassDevs(ref HIDClassGuid, null, IntPtr.Zero, 0x12);
+                SP_DEVINFO_DATA DeviceInfoData = new SP_DEVINFO_DATA();
+                SP_DEVICE_INTERFACE_DATA deviceData = new SP_DEVICE_INTERFACE_DATA();
+                HIDD_ATTRIBUTES DeviceAttributes = new HIDD_ATTRIBUTES();
+                SP_DEVICE_INTERFACE_DETAIL_DATA sP_DEVICE_INTERFACE_DETAIL_DATA = new SP_DEVICE_INTERFACE_DETAIL_DATA();
+
+                deviceData.cbSize = Marshal.SizeOf(deviceData);
+                DeviceInfoData.cbSize = (uint)Marshal.SizeOf(DeviceInfoData);
+                UInt16 index = 0;
+                UInt32 interfaceInfoSize = 0;
+                Boolean Found = false;
+                Boolean result = false;
+                //TODO add Check on Malloc etc...
+                //TODO explicit Call the W variant to Avoid Error
+                while (!Found && Native.SetupDiEnumDeviceInfo(DeviceInfoSet, index, ref DeviceInfoData))
+                {
+                    result = Native.SetupDiEnumDeviceInterfaces(DeviceInfoSet, IntPtr.Zero, ref HIDClassGuid, index, ref deviceData);
+                    result = Native.SetupDiGetDeviceInterfaceDetail(DeviceInfoSet, ref deviceData, IntPtr.Zero, 0, out interfaceInfoSize, IntPtr.Zero);
+
+                    IntPtr deviceInterfaceDetail = Marshal.AllocHGlobal((Int32)interfaceInfoSize);
+                    sP_DEVICE_INTERFACE_DETAIL_DATA.size = Marshal.SizeOf(sP_DEVICE_INTERFACE_DETAIL_DATA);
+                    Marshal.StructureToPtr(sP_DEVICE_INTERFACE_DETAIL_DATA, deviceInterfaceDetail, false);
+
+
+                    if (Native.SetupDiGetDeviceInterfaceDetail(DeviceInfoSet, ref deviceData, deviceInterfaceDetail, interfaceInfoSize, IntPtr.Zero, IntPtr.Zero))
+                    {
+                        String DevicePath = Marshal.PtrToStringUni(deviceInterfaceDetail + 4);
+
+                        var fileHandle = Native.CreateFileW(DevicePath, 0xC0000000, 3u, IntPtr.Zero, 3u, 0x80, IntPtr.Zero);
+                        if (fileHandle != new IntPtr(-1))
+                        {
+                            hid = new SafeFileHandle(fileHandle, true);
+                             if (Native.HidD_GetAttributes(hid, ref DeviceAttributes))
+                             {
+                                 if (DeviceAttributes.VendorID == 0x048D && (DeviceAttributes.ProductID & ProductIdMask) == (ProductId & ProductIdMask))
+                                 {
+                                     IntPtr PreparsedData = IntPtr.Zero;
+                                     HIDP_CAPS Caps = new HIDP_CAPS();
+                                     Native.HidD_GetPreparsedData(hid, ref PreparsedData);
+
+
+                                     Native.HidP_GetCaps(PreparsedData, out Caps);
+                                     Native.HidD_FreePreparsedData(PreparsedData);
+                                     if (Caps.FeatureReportByteLength == 33)
+                                     {
+                                         Found = true;
+                                         break;
+                                     }
+                                 }
+
+
+                             }
+                            hid = null;
+                        }
+                    }
+                    else
+                    {
+                        int error = Marshal.GetLastWin32Error();
+                    }
+
+                    Marshal.FreeHGlobal(deviceInterfaceDetail);
+                    index++;
+                }
+                Native.SetupDiDestroyDeviceInfoList(DeviceInfoSet);
+            }
+
+            return hid;
         }
     }
 }
---
+++ LenovoLegionToolkit.Lib/Utils/KeyboardStorage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LenovoLegionToolkit.Lib.Utils
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct LegionRGBKey
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public byte[] Header;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte Effect;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte Speed;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte Brightness;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] ZONE1_RGB;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] ZONE2_RGB;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] ZONE3_RGB;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public byte[] ZONE4_RGB;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte Padding;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte WAVE_LTR;
+        [MarshalAs(UnmanagedType.U1)]
+        public byte WAVE_RTL;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 13)]
+        public byte[] Unused;
+
+        public LegionRGBKey()
+        {
+            Header = new byte[2] { 0xcc, 0x16 };
+            Effect = 1;
+            Speed = 1;
+            Brightness = 1;
+            ZONE1_RGB = new byte[3] { 0xFF, 0xFF, 0xFF };
+            ZONE2_RGB = new byte[3] { 0xFF, 0xFF, 0xFF };
+            ZONE3_RGB = new byte[3] { 0xFF, 0xFF, 0xFF };
+            ZONE4_RGB = new byte[3] { 0xFF, 0xFF, 0xFF };
+            Padding = 0;
+            WAVE_LTR = 0;
+            WAVE_RTL = 0;
+            Unused = new byte[13];
+        }
+    }
+
+
+
+    //TODO Fix This, might not be Thread Safe
+    public sealed class KeyboardData
+    {
+        private static LegionRGBKey? _LegionRGBKey=null;
+
+        public static LegionRGBKey LegionRGBKey
+        {
+            get
+            {
+                if (_LegionRGBKey == null)
+                {
+                    _LegionRGBKey = new LegionRGBKey();
+                }
+                return (LegionRGBKey)_LegionRGBKey;
+            }
+
+            set
+            {
+                _LegionRGBKey = value;
+            }
+        }
+
+    }
+}
