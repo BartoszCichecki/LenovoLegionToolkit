652fd577e826f5d0008e49b76378a1c36201e279
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-21 23:39:42 +0200

    Added keyboard lightning event listener

---
+++ LenovoLegionToolkit.Lib/Listeners/KeyboardBacklightListener.cs
@@ -0,0 +1,14 @@
+﻿using System.Management;
+using System.Threading.Tasks;
+
+namespace LenovoLegionToolkit.Lib.Listeners
+{
+    public class KeyboardBacklightListener : AbstractWMIListener<KeyboardBacklight>
+    {
+        public KeyboardBacklightListener() : base("ROOT\\WMI", "LENOVO_GAMEZONE_LIGHT_PROFILE_CHANGE_EVENT") { }
+
+        protected override KeyboardBacklight GetValue(PropertyDataCollection properties) => default;
+
+        protected override Task OnChangedAsync(KeyboardBacklight value) => Task.CompletedTask;
+    }
+}
--- LenovoLegionToolkit.Lib/Listeners/SpecialKeyListener.cs
+++ LenovoLegionToolkit.Lib/Listeners/SpecialKeyListener.cs
@@ -7,11 +7,12 @@ namespace LenovoLegionToolkit.Lib.Listeners
     public class SpecialKeyListener : AbstractWMIListener<SpecialKey>
     {
         public SpecialKeyListener() : base("ROOT\\WMI", "LENOVO_UTILITY_EVENT") { }
+
         protected override SpecialKey GetValue(PropertyDataCollection properties)
         {
             var property = properties["PressTypeDataVal"];
             var propertyValue = Convert.ToInt32(property.Value);
-            var value = (SpecialKey)(object)(propertyValue);
+            var value = (SpecialKey)(object)propertyValue;
             return value;
         }
 
