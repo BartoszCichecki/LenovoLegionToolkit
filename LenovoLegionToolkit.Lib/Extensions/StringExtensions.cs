f8c593ef5e7b0718a6673a185faaada9c446e93c
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-23 16:17:12 +0200

    Show P state of GPU, disable button after deactivating.

---
+++ LenovoLegionToolkit.Lib/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LenovoLegionToolkit.Lib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetUntilOrEmpty(this string text, string stopAt)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+
+            int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+            if (charLocation > 0)
+                return text[..charLocation];
+
+            return string.Empty;
+        }
+    }
+}
