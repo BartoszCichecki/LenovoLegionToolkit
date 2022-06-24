5b2897f57b975352fab59ea7e64bb9797186c6b9
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-06-24 02:13:38 +0200

    Fn+R handling

--- LenovoLegionToolkit.Lib/Listeners/SpecialKeyListener.cs
+++ LenovoLegionToolkit.Lib/Listeners/SpecialKeyListener.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Management;
 using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.Features;
+using LenovoLegionToolkit.Lib.System;
 
 namespace LenovoLegionToolkit.Lib.Listeners
 {
     public class SpecialKeyListener : AbstractWMIListener<SpecialKey>
     {
-        public SpecialKeyListener() : base("ROOT\\WMI", "LENOVO_UTILITY_EVENT") { }
+        private readonly RefreshRateFeature _feature;
+
+        public SpecialKeyListener(RefreshRateFeature feature) : base("ROOT\\WMI", "LENOVO_UTILITY_EVENT")
+        {
+            _feature = feature;
+        }
 
         protected override SpecialKey GetValue(PropertyDataCollection properties)
         {
@@ -16,6 +23,38 @@ namespace LenovoLegionToolkit.Lib.Listeners
             return value;
         }
 
-        protected override Task OnChangedAsync(SpecialKey value) => Task.CompletedTask;
+        protected override Task OnChangedAsync(SpecialKey value)
+        {
+            if (value == SpecialKey.Fn_R)
+                return ToggleRefreshRateAsync();
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ToggleRefreshRateAsync()
+        {
+            try
+            {
+                if (await FnKeys.GetStatusAsync() == FnKeysStatus.Enabled)
+                    return;
+
+                var all = await _feature.GetAllStatesAsync().ConfigureAwait(false);
+                var current = await _feature.GetStateAsync().ConfigureAwait(false);
+
+                if (all is null || all.Length < 2)
+                    return;
+
+                var currentIndex = Array.IndexOf(all, current);
+                var newIndex = currentIndex + 1;
+
+                if (newIndex >= all.Length)
+                    newIndex = 0;
+
+                current = all[newIndex];
+
+                await _feature.SetStateAsync(current).ConfigureAwait(false);
+            }
+            catch { }
+        }
     }
 }
