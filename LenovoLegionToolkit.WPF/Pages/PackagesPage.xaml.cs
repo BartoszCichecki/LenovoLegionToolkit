48c8f95c2dbeeafdc2e8fd40a37083f1414b709b
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-07-01 00:39:06 +0200

    Download location and error handling for Downloads.

--- LenovoLegionToolkit.WPF/Pages/PackagesPage.xaml.cs
+++ LenovoLegionToolkit.WPF/Pages/PackagesPage.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Threading;
 using LenovoLegionToolkit.Lib;
-using LenovoLegionToolkit.Lib.Extensions;
+using LenovoLegionToolkit.Lib.System;
 using LenovoLegionToolkit.Lib.Utils;
 using LenovoLegionToolkit.WPF.Controls.Packages;
+using LenovoLegionToolkit.WPF.Utils;
 
 namespace LenovoLegionToolkit.WPF.Pages
 {
@@ -34,6 +39,7 @@ namespace LenovoLegionToolkit.WPF.Pages
 
             _machineTypeTextBox.Text = mi.MachineType;
             _osComboBox.SelectedIndex = os.Version >= new Version(10, 0, 22000, 0) ? 0 : 1;
+            _downloadToText.PlaceholderText = _downloadToText.Text = KnownFolders.GetPath(KnownFolder.Downloads);
 
             _downloadPackagesButton.IsEnabled = true;
             _cancelDownloadPackagesButton.IsEnabled = true;
@@ -45,8 +51,21 @@ namespace LenovoLegionToolkit.WPF.Pages
             _loader.Progress = value;
         });
 
+        private void DownloadToButton_Click(object sender, RoutedEventArgs e)
+        {
+            using var ofd = new FolderBrowserDialog
+            {
+                InitialDirectory = _downloadToText.Text,
+            };
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            _downloadToText.Text = ofd.SelectedPath;
+        }
+
         private async void DownloadPackagesButton_Click(object sender, RoutedEventArgs e)
         {
+            var errored = false;
             try
             {
                 _downloadPackagesButton.Visibility = Visibility.Collapsed;
@@ -55,13 +74,10 @@ namespace LenovoLegionToolkit.WPF.Pages
                 _loader.IsLoading = true;
                 _packages = null;
 
-                foreach (var control in _packagesStackPanel.Children.ToArray().OfType<PackageControl>())
-                    control.CancelDownloads();
-
                 _packagesStackPanel.Children.Clear();
                 _scrollViewer.ScrollToHome();
 
-                var machineType = _machineTypeTextBox.Text;
+                var machineType = _machineTypeTextBox.Text.Trim();
                 var os = _osComboBox.SelectedIndex switch
                 {
                     0 => "win11",
@@ -70,7 +86,10 @@ namespace LenovoLegionToolkit.WPF.Pages
                 };
 
                 if (string.IsNullOrWhiteSpace(machineType) || machineType.Length != 4 || string.IsNullOrWhiteSpace(os))
+                {
+                    await SnackbarHelper.ShowAsync("Something went wrong", "Check if Machine Type and OS are set correctly.");
                     return;
+                }
 
                 _getPackagesTokenSource?.Cancel();
                 _getPackagesTokenSource = new CancellationTokenSource();
@@ -85,11 +104,32 @@ namespace LenovoLegionToolkit.WPF.Pages
 
                 foreach (var package in packages)
                 {
-                    var control = new PackageControl(_packageDownloader, package);
+                    var control = new PackageControl(_packageDownloader, package, GetDownloadLocation);
                     _packagesStackPanel.Children.Add(control);
                 }
             }
-            catch (TaskCanceledException) { }
+            catch (TaskCanceledException)
+            {
+                errored = true;
+            }
+            catch (HttpRequestException ex)
+            {
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Error occured when downloading packages: {ex.Demystify()}");
+
+                SnackbarHelper.Show("Something went wrong", "Check if your internet connection is up and running.", true);
+
+                errored = true;
+            }
+            catch (Exception ex)
+            {
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Error occured when downloading packages: {ex.Demystify()}");
+
+                SnackbarHelper.Show("Something went wrong", ex.Message, true);
+
+                errored = true;
+            }
             finally
             {
                 _downloadPackagesButton.Visibility = Visibility.Visible;
@@ -97,6 +137,13 @@ namespace LenovoLegionToolkit.WPF.Pages
                 _loader.IsLoading = false;
                 _loader.Progress = 0;
                 _loader.IsIndeterminate = true;
+
+                if (errored)
+                {
+                    _packagesStackPanel.Children.Clear();
+                    _loader.Visibility = Visibility.Collapsed;
+                    _loader.IsLoading = true;
+                }
             }
         }
 
@@ -107,9 +154,6 @@ namespace LenovoLegionToolkit.WPF.Pages
             if (_packages is null)
                 return;
 
-            foreach (var control in _packagesStackPanel.Children.ToArray().OfType<PackageControl>())
-                control.CancelDownloads();
-
             _packagesStackPanel.Children.Clear();
             _scrollViewer.ScrollToHome();
 
@@ -117,9 +161,17 @@ namespace LenovoLegionToolkit.WPF.Pages
 
             foreach (var package in packages)
             {
-                var control = new PackageControl(_packageDownloader, package);
+                var control = new PackageControl(_packageDownloader, package, GetDownloadLocation);
                 _packagesStackPanel.Children.Add(control);
             }
+        }
+
+        private string GetDownloadLocation()
+        {
+            var location = _downloadToText.Text.Trim();
+            if (!Directory.Exists(location))
+                return KnownFolders.GetPath(KnownFolder.Downloads);
+            return location;
         }
 
         private List<Package> Sort(List<Package> packages)
