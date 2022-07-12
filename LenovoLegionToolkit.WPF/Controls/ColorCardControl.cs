4efa7f48bc9d677be8a9d55445d8d74e2ce08998
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-07-13 01:53:15 +0200

    Added RGB values to color picker.

--- LenovoLegionToolkit.WPF/Controls/ColorCardControl.cs
+++ LenovoLegionToolkit.WPF/Controls/ColorCardControl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -9,6 +10,7 @@ using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
 using Button = System.Windows.Controls.Button;
 using Color = System.Windows.Media.Color;
+using TextBox = Wpf.Ui.Controls.TextBox;
 
 namespace LenovoLegionToolkit.WPF.Controls
 {
@@ -25,10 +27,78 @@ namespace LenovoLegionToolkit.WPF.Controls
             Margin = new(0, 0, 8, 0),
         };
 
-        private readonly SquarePicker _colorPicker = new()
+        private readonly StackPanel _colorsPanel = new()
         {
             Width = 196,
+        };
+
+        private readonly SquarePicker _colorPicker = new()
+        {
             Height = 196,
+        };
+
+        private readonly Grid _rgbGrid = new()
+        {
+            ColumnDefinitions =
+            {
+                new() { Width = GridLength.Auto },
+                new() { Width = new(16, GridUnitType.Pixel) },
+                new() { Width = new(1, GridUnitType.Star) },
+            },
+            RowDefinitions =
+            {
+                new() { Height = GridLength.Auto },
+                new() { Height = GridLength.Auto },
+                new() { Height = GridLength.Auto },
+            },
+        };
+
+        private readonly Label _redLabel = new()
+        {
+            Content = "Red:",
+            VerticalContentAlignment = VerticalAlignment.Center,
+            Margin = new(0, 8, 0, 0),
+        };
+
+        private readonly Label _greenLabel = new()
+        {
+            Content = "Green:",
+            VerticalContentAlignment = VerticalAlignment.Center,
+            Margin = new(0, 8, 0, 0),
+        };
+
+        private readonly Label _blueLabel = new()
+        {
+            Content = "Blue:",
+            VerticalContentAlignment = VerticalAlignment.Center,
+            Margin = new(0, 8, 0, 0),
+        };
+
+        private readonly NumberBox _redTextBox = new()
+        {
+            Min = 0,
+            Max = 255,
+            IntegersOnly = true,
+            ClearButtonEnabled = false,
+            Margin = new(0, 8, 0, 0),
+        };
+
+        private readonly NumberBox _greenTextBox = new()
+        {
+            Min = 0,
+            Max = 255,
+            IntegersOnly = true,
+            ClearButtonEnabled = false,
+            Margin = new(0, 8, 0, 0),
+        };
+
+        private readonly NumberBox _blueTextBox = new()
+        {
+            Min = 0,
+            Max = 255,
+            IntegersOnly = true,
+            ClearButtonEnabled = false,
+            Margin = new(0, 8, 0, 0),
         };
 
         public SymbolRegular Icon
@@ -56,32 +126,84 @@ namespace LenovoLegionToolkit.WPF.Controls
         private void InitializeComponent()
         {
             _colorPicker.ColorChanged += ColorPicker_ColorChanged;
-            _colorPicker.MouseUp += ColorPicker_MouseUp;
+            _redTextBox.TextChanged += ColorTextBox_TextChanged;
+            _greenTextBox.TextChanged += ColorTextBox_TextChanged;
+            _blueTextBox.TextChanged += ColorTextBox_TextChanged;
+            _colorPicker.MouseUp += ColorsPanel_MouseUp;
 
             _colorButton.Click += (s, e) => _cardExpander.IsExpanded = !_cardExpander.IsExpanded;
             _cardHeaderControl.Accessory = _colorButton;
 
+            Grid.SetColumn(_redLabel, 0);
+            Grid.SetColumn(_greenLabel, 0);
+            Grid.SetColumn(_blueLabel, 0);
+            Grid.SetColumn(_redTextBox, 2);
+            Grid.SetColumn(_greenTextBox, 2);
+            Grid.SetColumn(_blueTextBox, 2);
+
+            Grid.SetRow(_redLabel, 0);
+            Grid.SetRow(_greenLabel, 1);
+            Grid.SetRow(_blueLabel, 2);
+            Grid.SetRow(_redTextBox, 0);
+            Grid.SetRow(_greenTextBox, 1);
+            Grid.SetRow(_blueTextBox, 2);
+
+            _rgbGrid.Children.Add(_redLabel);
+            _rgbGrid.Children.Add(_greenLabel);
+            _rgbGrid.Children.Add(_blueLabel);
+            _rgbGrid.Children.Add(_redTextBox);
+            _rgbGrid.Children.Add(_greenTextBox);
+            _rgbGrid.Children.Add(_blueTextBox);
+
+            _colorsPanel.Children.Add(_colorPicker);
+            _colorsPanel.Children.Add(_rgbGrid);
+
             _cardExpander.Header = _cardHeaderControl;
-            _cardExpander.Content = _colorPicker;
+            _cardExpander.Content = _colorsPanel;
 
             Content = _cardExpander;
         }
 
+        private void ColorTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var result = byte.TryParse(_redTextBox.Text, out var r);
+            result &= byte.TryParse(_greenTextBox.Text, out var g);
+            result &= byte.TryParse(_blueTextBox.Text, out var b);
+
+            if (!result)
+                return;
+
+            var color = Color.FromRgb(r, g, b);
+
+            if (Color.Equals(color, _colorPicker.SelectedColor))
+                return;
+
+            _colorPicker.SelectedColor = Color.FromRgb(r, g, b);
+
+            if (Mouse.LeftButton == MouseButtonState.Released && Mouse.RightButton == MouseButtonState.Released)
+                OnChanged?.Invoke(this, EventArgs.Empty);
+        }
+
         private void ColorPicker_ColorChanged(object sender, RoutedEventArgs e)
         {
-            _colorButton.Background = new SolidColorBrush(_colorPicker.SelectedColor);
+            var color = _colorPicker.SelectedColor;
+            _redTextBox.Text = color.R.ToString();
+            _greenTextBox.Text = color.G.ToString();
+            _blueTextBox.Text = color.B.ToString();
+            _colorButton.Background = new SolidColorBrush(color);
         }
 
-        private void ColorPicker_MouseUp(object sender, MouseButtonEventArgs e)
+        private void ColorsPanel_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            OnChanged?.Invoke(this, new EventArgs());
+            OnChanged?.Invoke(this, EventArgs.Empty);
         }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => OnChanged?.Invoke(this, EventArgs.Empty);
 
         public void SetColor(RGBColor color)
         {
             var c = Color.FromRgb(color.R, color.G, color.B);
+            _redTextBox.Text = color.R.ToString();
+            _greenTextBox.Text = color.G.ToString();
+            _blueTextBox.Text = color.B.ToString();
             _colorPicker.SelectedColor = c;
             _colorButton.Background = new SolidColorBrush(c);
             _colorButton.Visibility = Visibility.Visible;
