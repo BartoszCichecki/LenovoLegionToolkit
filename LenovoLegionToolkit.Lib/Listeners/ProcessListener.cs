94bfecc1de8f407c29de307c336cff89b53023a1
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-07-12 22:41:57 +0200

    Optimized process listener.

--- LenovoLegionToolkit.Lib/Listeners/ProcessListener.cs
+++ LenovoLegionToolkit.Lib/Listeners/ProcessListener.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Threading.Tasks;
@@ -8,11 +9,17 @@ namespace LenovoLegionToolkit.Lib.Listeners
 {
     public class ProcessListener : IListener<ProcessEventInfo>
     {
-        private static readonly int _pollRate = 2;
-
         private static readonly string[] _ignoredNames =
         {
+            "backgroundTaskHost.exe",
+            "CompPkgSrv.exe",
+            "conhost.exe",
+            "dllhost.exe",
+            "msedgewebview2",
             "msedgewebview2.exe",
+            "SearchProtocolHost.exe",
+            "svchost.exe",
+            "WmiPrvSE.exe",
         };
 
         private static readonly string[] _ignoredPaths =
@@ -27,47 +34,56 @@ namespace LenovoLegionToolkit.Lib.Listeners
 
         public ProcessListener()
         {
-            _instanceCreationListener = new InstanceEventListener(ProcessEventInfoType.Started, "__InstanceCreationEvent");
+            _instanceCreationListener = new InstanceEventListener(ProcessEventInfoType.Started, "Win32_ProcessStartTrace");
             _instanceCreationListener.Changed += InstanceListener_Changed;
 
-            _instanceDeletionListener = new InstanceEventListener(ProcessEventInfoType.Stopped, "__InstanceDeletionEvent");
+            _instanceDeletionListener = new InstanceEventListener(ProcessEventInfoType.Stopped, "Win32_ProcessStopTrace");
             _instanceDeletionListener.Changed += InstanceListener_Changed;
         }
 
-        private void InstanceListener_Changed(object? sender, ProcessEventInfo e)
+        private void InstanceListener_Changed(object? sender, (ProcessEventInfoType type, int processID, string processName) e)
         {
-            if (string.IsNullOrWhiteSpace(e.Process.Name) || _ignoredNames.Contains(e.Process.Name, StringComparer.InvariantCultureIgnoreCase))
-                return;
-            if (string.IsNullOrWhiteSpace(e.Process.ExecutablePath) || _ignoredPaths.Any(p => e.Process.ExecutablePath.StartsWith(p, StringComparison.InvariantCultureIgnoreCase)))
+            if (string.IsNullOrWhiteSpace(e.processName) || _ignoredNames.Contains(e.processName, StringComparer.InvariantCultureIgnoreCase))
                 return;
 
-            Changed?.Invoke(this, e);
+            if (e.processID < 0)
+                return;
+
+            var processPath = "";
+            try
+            {
+                processPath = Process.GetProcessById(e.processID).MainModule?.FileName;
+            }
+            catch (ArgumentException) { }
+            catch (InvalidOperationException) { }
+            catch (Win32Exception) { }
+
+            if (string.IsNullOrWhiteSpace(processPath) || _ignoredPaths.Any(p => processPath.StartsWith(p, StringComparison.InvariantCultureIgnoreCase)))
+                return;
+
+            Changed?.Invoke(this, new(e.type, new(e.processName, processPath)));
         }
 
-        private class InstanceEventListener : AbstractWMIListener<ProcessEventInfo>
+        private class InstanceEventListener : AbstractWMIListener<(ProcessEventInfoType, int, string)>
         {
             private readonly ProcessEventInfoType _type;
 
             public InstanceEventListener(ProcessEventInfoType type, string eventName)
-                : base("ROOT\\CIMV2", query: $"SELECT * FROM {eventName} WITHIN {_pollRate} WHERE TargetInstance ISA 'Win32_Process'")
+                : base("ROOT\\CIMV2", query: $"SELECT * FROM {eventName}")
             {
                 _type = type;
             }
 
-            protected override ProcessEventInfo GetValue(PropertyDataCollection properties)
+            protected override (ProcessEventInfoType, int, string) GetValue(PropertyDataCollection properties)
             {
-                if (properties["TargetInstance"].Value is not ManagementBaseObject mbo)
-                    return default;
+                var processName = properties["ProcessName"].Value?.ToString() ?? string.Empty;
+                if (!int.TryParse(properties["ProcessID"].Value?.ToString(), out int processID))
+                    processID = -1;
 
-                var processName = mbo.Properties["Name"].Value?.ToString() ?? string.Empty;
-                var executablePath = mbo.Properties["ExecutablePath"].Value?.ToString() ?? string.Empty;
-
-                processName = Path.GetFileNameWithoutExtension(processName);
-
-                return new(_type, new(processName, executablePath));
+                return (_type, processID, processName);
             }
 
-            protected override Task OnChangedAsync(ProcessEventInfo value) => Task.CompletedTask;
+            protected override Task OnChangedAsync((ProcessEventInfoType, int, string) value) => Task.CompletedTask;
         }
     }
 }
