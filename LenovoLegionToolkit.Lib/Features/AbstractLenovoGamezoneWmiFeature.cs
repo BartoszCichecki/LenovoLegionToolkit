884d6def12eac35d9bad8bc082b8afecbd80e029
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-08-14 23:26:49 +0200

    WMI refactor

--- LenovoLegionToolkit.Lib/Features/AbstractLenovoGamezoneWmiFeature.cs
+++ LenovoLegionToolkit.Lib/Features/AbstractLenovoGamezoneWmiFeature.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management;
 using System.Threading.Tasks;
-using LenovoLegionToolkit.Lib.Extensions;
+using LenovoLegionToolkit.Lib.System;
 using LenovoLegionToolkit.Lib.Utils;
 
 namespace LenovoLegionToolkit.Lib.Features
 {
     public abstract class AbstractLenovoGamezoneWmiFeature<T> : IFeature<T> where T : struct, Enum, IComparable
     {
+        private static readonly string _scope = @"root\WMI";
+        private static readonly FormattableString _query = $"SELECT * FROM LENOVO_GAMEZONE_DATA";
+
         private readonly string _methodNameSuffix;
         private readonly int _offset;
         private readonly string? _supportMethodName;
@@ -21,7 +21,7 @@ namespace LenovoLegionToolkit.Lib.Features
             int offset,
             string? supportMethodName = null,
             int supportOffset = 0,
-            string inParameterName = "Value",
+            string inParameterName = "Data",
             string outParameterName = "Data")
         {
             _methodNameSuffix = methodNameSuffix;
@@ -39,7 +39,11 @@ namespace LenovoLegionToolkit.Lib.Features
                 if (_supportMethodName is null)
                     return true;
 
-                var value = await ExecuteGamezoneAsync(_supportMethodName, _outParameterName).ConfigureAwait(false);
+                var value = await WMI.CallAsync(_scope,
+                    _query,
+                    _supportMethodName,
+                    new(),
+                    pdc => Convert.ToInt32(pdc[_outParameterName].Value)).ConfigureAwait(false);
                 return value > _supportOffset;
             }
             catch
@@ -63,7 +67,12 @@ namespace LenovoLegionToolkit.Lib.Features
                 throw new NotSupportedException($"Feature {_methodNameSuffix} is not supported.");
             }
 
-            var result = FromInternal(await ExecuteGamezoneAsync("Get" + _methodNameSuffix, _outParameterName).ConfigureAwait(false));
+            var internalResult = await WMI.CallAsync(_scope,
+                _query,
+                "Get" + _methodNameSuffix,
+                new(),
+                pdc => Convert.ToInt32(pdc[_outParameterName].Value)).ConfigureAwait(false);
+            var result = FromInternal(internalResult);
 
             if (Log.Instance.IsTraceEnabled)
                 Log.Instance.Trace($"State is {result} [feature={GetType().Name}]");
@@ -76,56 +85,17 @@ namespace LenovoLegionToolkit.Lib.Features
             if (Log.Instance.IsTraceEnabled)
                 Log.Instance.Trace($"Setting state to {state}... [feature={GetType().Name}]");
 
-            var result = await ExecuteGamezoneAsync("Set" + _methodNameSuffix, _outParameterName,
-                new Dictionary<string, string>
-                {
-                    {_inParameterName, ToInternal(state).ToString()}
-                }).ConfigureAwait(false);
+            await WMI.CallAsync(_scope,
+                _query,
+                "Set" + _methodNameSuffix,
+                new() { { _inParameterName, ToInternal(state).ToString() } });
 
             if (Log.Instance.IsTraceEnabled)
-                Log.Instance.Trace($"Set state to {state} [feature={GetType().Name}, result={result}]");
+                Log.Instance.Trace($"Set state to {state} [feature={GetType().Name}]");
         }
 
         private int ToInternal(T state) => (int)(object)state + _offset;
 
         private T FromInternal(int state) => (T)(object)(state - _offset);
-
-        private Task<int> ExecuteGamezoneAsync(string methodName, string resultPropertyName, Dictionary<string, string>? methodParams = null) =>
-            ExecuteAsync("SELECT * FROM LENOVO_GAMEZONE_DATA", methodName, resultPropertyName, methodParams);
-
-        private async Task<int> ExecuteAsync(string queryString,
-            string methodName,
-            string resultPropertyName,
-            Dictionary<string, string>? methodParams = null)
-        {
-            if (Log.Instance.IsTraceEnabled)
-                Log.Instance.Trace($"Executing WMI query... [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
-
-            var mos = new ManagementObjectSearcher("ROOT\\WMI", queryString);
-            var managementObjects = await mos.GetAsync().ConfigureAwait(false);
-            var managementObject = managementObjects.FirstOrDefault();
-
-            if (managementObject is null)
-            {
-                if (Log.Instance.IsTraceEnabled)
-                    Log.Instance.Trace($"No results in query [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
-
-                throw new InvalidOperationException("No results in query");
-            }
-
-            var mo = (ManagementObject)managementObject;
-            var methodParamsObject = mo.GetMethodParameters(methodName);
-            if (methodParams is not null)
-                foreach (var pair in methodParams)
-                    methodParamsObject[pair.Key] = pair.Value;
-
-            var result = mo.InvokeMethod(methodName, methodParamsObject, null)?.Properties[resultPropertyName].Value;
-            var intResult = Convert.ToInt32(result);
-
-            if (Log.Instance.IsTraceEnabled)
-                Log.Instance.Trace($"Executed WMI query with result {intResult} [feature={GetType().Name}, queryString={queryString}, methodName={methodName}, resultPropertyName={resultPropertyName}, methodParams.Count={methodParams?.Count}]");
-
-            return intResult;
-        }
     }
 }
