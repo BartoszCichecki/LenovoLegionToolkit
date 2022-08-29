d8786ae8d7ef824a272139926c8dbaa2a0747f93
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-08-29 21:02:36 +0200

    Added time trigger.

--- LenovoLegionToolkit.Lib.Automation/IAutomationEvent.cs
+++ LenovoLegionToolkit.Lib.Automation/IAutomationEvent.cs
@@ -10,4 +10,9 @@
     {
         public ProcessEventInfo ProcessEventInfo { get; init; }
     }
+
+    public struct TimeAutomationEvent : IAutomationEvent
+    {
+        public Time Time { get; init; }
+    }
 }
