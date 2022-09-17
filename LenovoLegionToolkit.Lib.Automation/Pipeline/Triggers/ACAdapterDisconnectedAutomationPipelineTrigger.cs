11f4aa0744c1ee50f49b500204366f16d79e4d50
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-09-17 20:54:15 +0200

    Added low wattage AC adapter action on supported models.

--- LenovoLegionToolkit.Lib.Automation/Pipeline/Triggers/ACAdapterDisconnectedAutomationPipelineTrigger.cs
+++ LenovoLegionToolkit.Lib.Automation/Pipeline/Triggers/ACAdapterDisconnectedAutomationPipelineTrigger.cs
@@ -8,7 +8,7 @@ namespace LenovoLegionToolkit.Lib.Automation.Pipeline.Triggers
     public class ACAdapterDisconnectedAutomationPipelineTrigger : IAutomationPipelineTrigger, IPowerAutomationPipelineTrigger, IDisallowDuplicatesAutomationPipelineTrigger
     {
         [JsonIgnore]
-        public string DisplayName => "When on battery power";
+        public string DisplayName => "When AC power adapter is connected";
 
         public async Task<bool> IsSatisfiedAsync(IAutomationEvent automationEvent)
         {
---
+++ LenovoLegionToolkit.Lib.Automation/Pipeline/Triggers/LowWattageACAdapterConnectedAutomationPipelineTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.System;
+using Newtonsoft.Json;
+
+namespace LenovoLegionToolkit.Lib.Automation.Pipeline.Triggers
+{
+    public class LowWattageACAdapterConnectedAutomationPipelineTrigger : IAutomationPipelineTrigger, IPowerAutomationPipelineTrigger, IDisallowDuplicatesAutomationPipelineTrigger
+    {
+        [JsonIgnore]
+        public string DisplayName => "When low wattage AC power adapter is connected";
+
+        public async Task<bool> IsSatisfiedAsync(IAutomationEvent automationEvent)
+        {
+            if (automationEvent is not (PowerAutomationEvent or StartupAutomationEvent))
+                return false;
+
+            return await Power.IsPowerAdapterConnectedAsync().ConfigureAwait(false) == PowerAdapterStatus.ConnectedLowWattage;
+        }
+
+        public IAutomationPipelineTrigger DeepCopy() => new LowWattageACAdapterConnectedAutomationPipelineTrigger();
+
+        public override bool Equals(object? obj) => obj is LowWattageACAdapterConnectedAutomationPipelineTrigger;
+
+        public override int GetHashCode() => HashCode.Combine(DisplayName);
+    }
+}
