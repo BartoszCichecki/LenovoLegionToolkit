41287e82fb0135f549e306f002823184e39426b0
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-09-22 20:58:19 +0200

    Moved donations to separate page.

---
+++ LenovoLegionToolkit.WPF/Pages/DonatePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using LenovoLegionToolkit.WPF.Extensions;
+
+namespace LenovoLegionToolkit.WPF.Pages
+{
+    public partial class DonatePage
+    {
+        public DonatePage()
+        {
+            InitializeComponent();
+        }
+
+        private void DonateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Constants.PayPalUri.Open();
+            e.Handled = true;
+        }
+    }
+}
