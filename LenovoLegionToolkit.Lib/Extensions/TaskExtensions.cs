2cc53643532fe8c8126aafc6c2018d7250e924ec
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-09-24 20:16:39 +0200

    Fix #212

---
+++ LenovoLegionToolkit.Lib/Extensions/TaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace LenovoLegionToolkit.Lib.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static ValueTask AsValueTask(this Task task) => new(task);
+    }
+}
--- LenovoLegionToolkit.Lib/IoCModule.cs
+++ LenovoLegionToolkit.Lib/IoCModule.cs
@@ -38,29 +38,32 @@ namespace LenovoLegionToolkit.Lib
             builder.Register<WhiteKeyboardBacklightFeature>();
             builder.Register<WinKeyFeature>();
 
+            builder.Register<PowerStateListener>()
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
+                .AutoActivate();
             builder.Register<PowerModeListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<DisplayConfigurationListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<DriverKeyListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<PowerPlanListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<RGBKeyboardBacklightListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<SpecialKeyListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<WhiteKeyboardBacklightListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
             builder.Register<WinKeyListener>()
-                .OnActivating(e => e.Instance.StartAsync())
+                .OnActivating(e => e.Instance.StartAsync().AsValueTask())
                 .AutoActivate();
 
             builder.Register<GPUController>();
