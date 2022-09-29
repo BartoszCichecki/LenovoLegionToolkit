17f91c1a132c72b9b1ab0004ce2544f584ca0d92
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-09-29 21:23:05 +0200

    Added AI mode controller

---
+++ LenovoLegionToolkit.Lib/Settings/BalanceModeSettings.cs
@@ -0,0 +1,16 @@
+﻿using static LenovoLegionToolkit.Lib.Settings.BalanceModeSettings;
+
+namespace LenovoLegionToolkit.Lib.Settings
+{
+    public class BalanceModeSettings : AbstractSettings<BalanceModeSettingsStore>
+    {
+        public class BalanceModeSettingsStore
+        {
+            public bool AIModeEnabled { get; set; }
+        }
+
+        public override BalanceModeSettingsStore Default => new();
+
+        protected override string FileName => "balancemode.json";
+    }
+}
