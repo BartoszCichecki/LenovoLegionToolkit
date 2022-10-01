4c8f05850b89f119063eda8a0e060af516b423f0
jduan <jiawei.duan@outlook.com>
2022-09-30 20:51:38 -0700

    Possible fix for #236

--- LenovoLegionToolkit.Lib/Features/IGPUModeFeature.cs
+++ LenovoLegionToolkit.Lib/Features/IGPUModeFeature.cs
@@ -2,6 +2,6 @@
 {
     public class IGPUModeFeature : AbstractLenovoGamezoneWmiFeature<IGPUModeState>
     {
-        public IGPUModeFeature() : base("IGPUModeStatus", 0, "IsSupportIGPUMode", inParameterName: "mode") { }
+        public IGPUModeFeature() : base("IGPUModeStatus", 0, "IsSupportIGPUMode", inParameterName: "mode", notifyUpdateMethodName: "NotifyDGPUStatus", notifyUpdateInParameterNameName: "Status") { }
     }
 }
