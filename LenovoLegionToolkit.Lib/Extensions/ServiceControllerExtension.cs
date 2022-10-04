8559a45a9060b4729b80909e93946522fafd772d
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-05 00:31:16 +0200

    Moved advapi to vanara

--- LenovoLegionToolkit.Lib/Extensions/ServiceControllerExtension.cs
+++ LenovoLegionToolkit.Lib/Extensions/ServiceControllerExtension.cs
@@ -1,62 +1,52 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
-using LenovoLegionToolkit.Lib.System;
+using System.ServiceProcess;
+using Vanara.PInvoke;
 
-namespace System.ServiceProcess
+namespace LenovoLegionToolkit.Lib.Extensions
 {
     internal static class ServiceControllerExtension
     {
-        private const uint SERVICE_NO_CHANGE = 0xFFFFFFFF;
-        private const uint SERVICE_QUERY_CONFIG = 0x00000001;
-        private const uint SERVICE_CHANGE_CONFIG = 0x00000002;
-        private const uint SC_MANAGER_ALL_ACCESS = 0x000F003F;
-
-        public static void ChangeStartMode(this ServiceController svc, ServiceStartMode mode)
+        public static void ChangeStartMode(this ServiceController svc, bool enabled)
         {
-            var scManagerHandle = IntPtr.Zero;
-            var serviceHandle = IntPtr.Zero;
+            var scManagerHandle = new AdvApi32.SafeSC_HANDLE(IntPtr.Zero);
+            var serviceHandle = new AdvApi32.SafeSC_HANDLE(IntPtr.Zero);
 
             try
             {
-                scManagerHandle = Native.OpenSCManager(null, null, SC_MANAGER_ALL_ACCESS);
+                scManagerHandle = AdvApi32.OpenSCManager(null, null, AdvApi32.ScManagerAccessTypes.SC_MANAGER_ALL_ACCESS);
                 if (scManagerHandle == IntPtr.Zero)
                     throw new ExternalException("Open Service Manager Error");
 
-                serviceHandle = Native.OpenService(
+                serviceHandle = AdvApi32.OpenService(
                     scManagerHandle,
                     svc.ServiceName,
-                    SERVICE_QUERY_CONFIG | SERVICE_CHANGE_CONFIG);
+                    AdvApi32.ServiceAccessTypes.SERVICE_CHANGE_CONFIG | AdvApi32.ServiceAccessTypes.SERVICE_CHANGE_CONFIG);
 
                 if (serviceHandle == IntPtr.Zero)
                     throw new ExternalException("Open Service Error");
 
-                var result = Native.ChangeServiceConfig(
+                var result = AdvApi32.ChangeServiceConfig(
                     serviceHandle,
-                    SERVICE_NO_CHANGE,
-                    (uint)mode,
-                    SERVICE_NO_CHANGE,
-                    null,
-                    null,
-                    IntPtr.Zero,
-                    null,
-                    null,
-                    null,
-                    null);
+                    AdvApi32.ServiceTypes.SERVICE_NO_CHANGE,
+                    enabled ? AdvApi32.ServiceStartType.SERVICE_AUTO_START : AdvApi32.ServiceStartType.SERVICE_DISABLED,
+                    AdvApi32.ServiceErrorControlType.SERVICE_NO_CHANGE);
 
-                if (result == false)
-                {
-                    var nError = Marshal.GetLastWin32Error();
-                    var win32Exception = new Win32Exception(nError);
-                    throw new ExternalException($"Could not change service start type: {win32Exception.Message}");
-                }
+                if (result)
+                    return;
+
+                var nError = Marshal.GetLastWin32Error();
+                var win32Exception = new Win32Exception(nError);
+                throw new ExternalException($"Could not change service start type: {win32Exception.Message}");
 
             }
             finally
             {
                 if (serviceHandle != IntPtr.Zero)
-                    _ = Native.CloseServiceHandle(serviceHandle);
+                    AdvApi32.CloseServiceHandle(serviceHandle);
                 if (scManagerHandle != IntPtr.Zero)
-                    _ = Native.CloseServiceHandle(scManagerHandle);
+                    AdvApi32.CloseServiceHandle(scManagerHandle);
             }
         }
     }
