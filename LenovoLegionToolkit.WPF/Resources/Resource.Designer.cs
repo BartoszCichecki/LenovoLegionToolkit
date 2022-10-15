c7558fce373010592e1a7822a54396aeb62bdb2e
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-15 23:22:18 +0200

    Clean up

--- LenovoLegionToolkit.WPF/Resources/Resource.Designer.cs
+++ LenovoLegionToolkit.WPF/Resources/Resource.Designer.cs
@@ -124,7 +124,7 @@ namespace LenovoLegionToolkit.WPF.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to    .
         /// </summary>
         public static string AboutPage_TranslationCredit {
             get {
