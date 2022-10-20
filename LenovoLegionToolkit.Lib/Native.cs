3b1fc7bdb6dd47480e391f09e2ca428f7c096ef7
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-20 23:00:49 +0200

    Added brightness/profile structs + device handle

--- LenovoLegionToolkit.Lib/Native.cs
+++ LenovoLegionToolkit.Lib/Native.cs
@@ -36,4 +36,60 @@ namespace LenovoLegionToolkit.Lib
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 13)]
         public byte[] Unused;
     }
+
+    internal enum LENOVO_SPECTRUM_OPERATION_TYPE : byte
+    {
+        ProfileSet1 = 0xC8,
+        EffectChange = 0xCB,
+        ProfileSet2 = 0xCC,
+        Brightness = 0xCE,
+        AuroraSendBitmap = 0xA1
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal struct LENOVO_SPECTRUM_HEADER
+    {
+        public byte Head = 7;
+        public LENOVO_SPECTRUM_OPERATION_TYPE OperationType;
+        public byte PacketSize; // %256
+        public byte Tail = 3;
+
+        public LENOVO_SPECTRUM_HEADER(LENOVO_SPECTRUM_OPERATION_TYPE operationType, int packetSize) : this()
+        {
+            OperationType = operationType;
+            PacketSize = (byte)(packetSize % 256);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 960)]
+    internal struct LENOVO_SPECTRUM_SET_BRIGHTHNESS
+    {
+        public LENOVO_SPECTRUM_HEADER Header;
+        public byte Brightness;
+
+        public LENOVO_SPECTRUM_SET_BRIGHTHNESS(byte brightness)
+        {
+            Header = new(LENOVO_SPECTRUM_OPERATION_TYPE.Brightness, 0xC0);
+            Brightness = brightness;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = 960)]
+    internal struct LENOVO_SPECTRUM_SET_PROFILE
+    {
+        public LENOVO_SPECTRUM_HEADER Header;
+        public byte Profile;
+
+        public LENOVO_SPECTRUM_SET_PROFILE(byte profile, bool first)
+        {
+            Header = new(first ? LENOVO_SPECTRUM_OPERATION_TYPE.ProfileSet1 : LENOVO_SPECTRUM_OPERATION_TYPE.ProfileSet2, 0xC0);
+            Profile = profile;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = 960)]
+    internal struct LENOVO_SPECTRUM_SET_EFFECT
+    {
+        public LENOVO_SPECTRUM_HEADER Header;
+    }
 }
--- LenovoLegionToolkit.Lib/System/Devices.cs
+++ LenovoLegionToolkit.Lib/System/Devices.cs
@@ -8,21 +8,24 @@ using Windows.Win32.Devices.DeviceAndDriverInstallation;
 using Windows.Win32.Foundation;
 using Windows.Win32.Storage.FileSystem;
 
+#pragma warning disable CA1416 // Validate platform compatibility
+
 namespace LenovoLegionToolkit.Lib.System
 {
     internal static class Devices
     {
-        private static readonly object _locker = new();
+        private static readonly object Lock = new();
 
         private static SafeFileHandle? _battery;
         private static SafeFileHandle? _rgbKeyboard;
+        private static SafeFileHandle? _spectrumRgbKeyboard;
 
         public static unsafe SafeFileHandle GetBattery()
         {
             if (_battery is not null)
                 return _battery;
 
-            lock (_locker)
+            lock (Lock)
             {
                 if (_battery is not null)
                     return _battery;
@@ -80,106 +83,135 @@ namespace LenovoLegionToolkit.Lib.System
             return _battery;
         }
 
-        public static unsafe SafeFileHandle? GetRGBKeyboard()
+        public static SafeFileHandle? GetRGBKeyboard()
         {
             if (_rgbKeyboard is not null)
                 return _rgbKeyboard;
 
-            lock (_locker)
+            lock (Lock)
             {
                 if (_rgbKeyboard is not null)
                     return _rgbKeyboard;
 
-                var vendorId = 0x048D;
-                var productIdMasked = 0xC900;
-                var productIdMask = 0xFF00;
-                var descriptorLength = 0x21;
+                const ushort vendorId = 0x048D;
+                const ushort productIdMasked = 0xC900;
+                const ushort productIdMask = 0xFF00;
+                const ushort descriptorLength = 0x21;
 
-                PInvoke.HidD_GetHidGuid(out var devClassHidGuid);
-
-                var deviceHandle = PInvoke.SetupDiGetClassDevs(devClassHidGuid,
-                    null,
-                    HWND.Null,
-                    PInvoke.DIGCF_PRESENT | PInvoke.DIGCF_DEVICEINTERFACE);
-
-                uint index = 0;
-                while (true)
-                {
-                    var currentIndex = index;
-                    index++;
-
-                    var deviceInfoData = new SP_DEVINFO_DATA { cbSize = (uint)Marshal.SizeOf<SP_DEVINFO_DATA>() };
-                    var result1 = PInvoke.SetupDiEnumDeviceInfo(deviceHandle, currentIndex, ref deviceInfoData);
-                    if (!result1)
-                    {
-                        if (Marshal.GetLastWin32Error() == PInvokeExtensions.ERROR_NO_MORE_ITEMS)
-                            break;
-
-                        PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInfo");
-                    }
-
-                    var deviceInterfaceData = new SP_DEVICE_INTERFACE_DATA { cbSize = (uint)Marshal.SizeOf<SP_DEVICE_INTERFACE_DATA>() };
-
-                    var result2 = PInvoke.SetupDiEnumDeviceInterfaces(deviceHandle, null, devClassHidGuid, currentIndex, ref deviceInterfaceData);
-                    if (!result2)
-                        PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInterfaces");
-
-                    var requiredSize = 0u;
-                    _ = PInvoke.SetupDiGetDeviceInterfaceDetail(deviceHandle, deviceInterfaceData, null, 0, &requiredSize, null);
-
-                    string devicePath;
-                    var output = IntPtr.Zero;
-                    try
-                    {
-                        output = Marshal.AllocHGlobal((int)requiredSize);
-                        var deviceDetailData = (SP_DEVICE_INTERFACE_DETAIL_DATA_W*)output.ToPointer();
-                        deviceDetailData->cbSize = (uint)Marshal.SizeOf<SP_DEVICE_INTERFACE_DETAIL_DATA_W>();
-
-                        var result3 = PInvoke.SetupDiGetDeviceInterfaceDetail(deviceHandle, deviceInterfaceData, deviceDetailData, requiredSize, null, null);
-                        if (!result3)
-                            PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInterfaces");
-
-                        devicePath = new string(&deviceDetailData->DevicePath._0);
-                    }
-                    finally
-                    {
-                        Marshal.FreeHGlobal(output);
-                    }
-
-                    var fileHandle = PInvoke.CreateFile(devicePath,
-                        FILE_ACCESS_FLAGS.FILE_READ_DATA | FILE_ACCESS_FLAGS.FILE_WRITE_DATA,
-                        FILE_SHARE_MODE.FILE_SHARE_READ | FILE_SHARE_MODE.FILE_SHARE_WRITE,
-                        null,
-                        FILE_CREATION_DISPOSITION.OPEN_EXISTING,
-                        FILE_FLAGS_AND_ATTRIBUTES.FILE_ATTRIBUTE_NORMAL,
-                        null);
-
-                    if (!PInvoke.HidD_GetAttributes(fileHandle, out var hiddAttributes))
-                        continue;
-
-                    nint preParsedData = 0;
-                    try
-                    {
-                        PInvoke.HidD_GetPreparsedData(fileHandle, out preParsedData);
-                        PInvoke.HidP_GetCaps(preParsedData, out var caps);
-
-                        if (Log.Instance.IsTraceEnabled)
-                            Log.Instance.Trace($"Checking device... [vendorId={hiddAttributes.VendorID:X2}, productId={hiddAttributes.ProductID:X2}, descriptorLength={caps.FeatureReportByteLength}]");
-
-                        if (hiddAttributes.VendorID == vendorId && (hiddAttributes.ProductID & productIdMask) == productIdMasked && caps.FeatureReportByteLength == descriptorLength)
-                        {
-                            _rgbKeyboard = fileHandle;
-                            break;
-                        }
-                    }
-                    finally
-                    {
-                        PInvoke.HidD_FreePreparsedData(preParsedData);
-                    }
-                }
+                _rgbKeyboard = FindHidDevice(vendorId, productIdMask, productIdMasked, descriptorLength);
             }
 
             return _rgbKeyboard;
+        }
+
+        public static SafeFileHandle? GetSpectrumRGBKeyboard()
+        {
+            if (_spectrumRgbKeyboard is not null)
+                return _spectrumRgbKeyboard;
+
+            lock (Lock)
+            {
+                if (_spectrumRgbKeyboard is not null)
+                    return _spectrumRgbKeyboard;
+
+                const ushort vendorId = 0x048D;
+                const ushort productIdMasked = 0xC900;
+                const ushort productIdMask = 0xFF00;
+                const ushort descriptorLength = 0x03C0;
+
+                _spectrumRgbKeyboard = FindHidDevice(vendorId, productIdMask, productIdMasked, descriptorLength);
+            }
+
+            return _spectrumRgbKeyboard;
+        }
+
+        private static unsafe SafeFileHandle? FindHidDevice(ushort vendorId, ushort productIdMask, ushort productIdMasked, ushort descriptorLength)
+        {
+            PInvoke.HidD_GetHidGuid(out var devClassHidGuid);
+
+            var deviceHandle = PInvoke.SetupDiGetClassDevs(devClassHidGuid,
+                null,
+                HWND.Null,
+                PInvoke.DIGCF_PRESENT | PInvoke.DIGCF_DEVICEINTERFACE);
+
+            uint index = 0;
+            while (true)
+            {
+                var currentIndex = index;
+                index++;
+
+                var deviceInfoData = new SP_DEVINFO_DATA { cbSize = (uint)Marshal.SizeOf<SP_DEVINFO_DATA>() };
+                var result1 = PInvoke.SetupDiEnumDeviceInfo(deviceHandle, currentIndex, ref deviceInfoData);
+                if (!result1)
+                {
+                    if (Marshal.GetLastWin32Error() == PInvokeExtensions.ERROR_NO_MORE_ITEMS)
+                        break;
+
+                    PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInfo");
+                }
+
+                var deviceInterfaceData = new SP_DEVICE_INTERFACE_DATA
+                { cbSize = (uint)Marshal.SizeOf<SP_DEVICE_INTERFACE_DATA>() };
+
+                var result2 = PInvoke.SetupDiEnumDeviceInterfaces(deviceHandle, null, devClassHidGuid, currentIndex,
+                    ref deviceInterfaceData);
+                if (!result2)
+                    PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInterfaces");
+
+                var requiredSize = 0u;
+                _ = PInvoke.SetupDiGetDeviceInterfaceDetail(deviceHandle, deviceInterfaceData, null, 0, &requiredSize, null);
+
+                string devicePath;
+                var output = IntPtr.Zero;
+                try
+                {
+                    output = Marshal.AllocHGlobal((int)requiredSize);
+                    var deviceDetailData = (SP_DEVICE_INTERFACE_DETAIL_DATA_W*)output.ToPointer();
+                    deviceDetailData->cbSize = (uint)Marshal.SizeOf<SP_DEVICE_INTERFACE_DETAIL_DATA_W>();
+
+                    var result3 = PInvoke.SetupDiGetDeviceInterfaceDetail(deviceHandle, deviceInterfaceData, deviceDetailData,
+                        requiredSize, null, null);
+                    if (!result3)
+                        PInvokeExtensions.ThrowIfWin32Error("SetupDiEnumDeviceInterfaces");
+
+                    devicePath = new string(&deviceDetailData->DevicePath._0);
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(output);
+                }
+
+                var fileHandle = PInvoke.CreateFile(devicePath,
+                    FILE_ACCESS_FLAGS.FILE_READ_DATA | FILE_ACCESS_FLAGS.FILE_WRITE_DATA,
+                    FILE_SHARE_MODE.FILE_SHARE_READ | FILE_SHARE_MODE.FILE_SHARE_WRITE,
+                    null,
+                    FILE_CREATION_DISPOSITION.OPEN_EXISTING,
+                    FILE_FLAGS_AND_ATTRIBUTES.FILE_ATTRIBUTE_NORMAL,
+                    null);
+
+                if (!PInvoke.HidD_GetAttributes(fileHandle, out var hiddAttributes))
+                    continue;
+
+                nint preParsedData = 0;
+                try
+                {
+                    PInvoke.HidD_GetPreparsedData(fileHandle, out preParsedData);
+                    PInvoke.HidP_GetCaps(preParsedData, out var caps);
+
+                    if (Log.Instance.IsTraceEnabled)
+                        Log.Instance.Trace(
+                            $"Checking device... [vendorId={hiddAttributes.VendorID:X2}, productId={hiddAttributes.ProductID:X2}, descriptorLength={caps.FeatureReportByteLength}]");
+
+                    if (hiddAttributes.VendorID == vendorId && (hiddAttributes.ProductID & productIdMask) == productIdMasked && caps.FeatureReportByteLength == descriptorLength)
+                        return fileHandle;
+                }
+                finally
+                {
+                    PInvoke.HidD_FreePreparsedData(preParsedData);
+                }
+            }
+
+            return null;
         }
     }
 }
