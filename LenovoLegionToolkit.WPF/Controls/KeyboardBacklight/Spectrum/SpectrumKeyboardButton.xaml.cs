0044847131aca007aea1c6a478990edbb56a2558
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-26 19:31:33 +0200

    Added mockups for spectrum

---
+++ LenovoLegionToolkit.WPF/Controls/KeyboardBacklight/Spectrum/SpectrumKeyboardButton.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LenovoLegionToolkit.WPF.Controls.KeyboardBacklight.Spectrum
+{
+    public partial class SpectrumKeyboardButton
+    {
+        public SpectrumKeyboardButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
