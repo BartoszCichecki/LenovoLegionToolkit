baf227a2ce5e086719b87ffd0b3cc043962a91f2
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-27 01:31:16 +0200

    Scaling of device control

--- LenovoLegionToolkit.WPF/Controls/KeyboardBacklight/Spectrum/SpectrumKeyboardBacklightControl.xaml.cs
+++ LenovoLegionToolkit.WPF/Controls/KeyboardBacklight/Spectrum/SpectrumKeyboardBacklightControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace LenovoLegionToolkit.WPF.Controls.KeyboardBacklight.Spectrum
 {
@@ -9,6 +11,20 @@ namespace LenovoLegionToolkit.WPF.Controls.KeyboardBacklight.Spectrum
         public SpectrumKeyboardBacklightControl()
         {
             InitializeComponent();
+
+            SizeChanged += SpectrumKeyboardBacklightControl_SizeChanged;
+        }
+
+        private void SpectrumKeyboardBacklightControl_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            if (_device.LayoutTransform is not ScaleTransform scaleTransform)
+                return;
+
+            var target = (0.75 * ActualWidth) / _device.ActualWidth;
+            var scale = Math.Clamp(target, 0.5, 1.5);
+
+            scaleTransform.ScaleX = scale;
+            scaleTransform.ScaleY = scale;
         }
 
         protected override Task OnRefreshAsync() => Task.CompletedTask;
