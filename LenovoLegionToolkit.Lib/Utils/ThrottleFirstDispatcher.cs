12b510e99ce973ce59cc1faed7ead1d0a23f02fa
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-29 17:31:19 +0200

    Replaced debounce with throttle first, timespans clean up
    (cherry picked from commit 6e451d21124c137abe5b86f01325eeee58cd8731)

---
+++ LenovoLegionToolkit.Lib/Utils/ThrottleFirstDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LenovoLegionToolkit.Lib.Utils
+{
+    public class ThrottleFirstDispatcher
+    {
+        private readonly object _lock = new();
+
+        private readonly TimeSpan _interval;
+
+        private DateTime _lastEvent = DateTime.MinValue;
+
+        public ThrottleFirstDispatcher(TimeSpan interval)
+        {
+            _interval = interval;
+        }
+
+        public Task DispatchAsync(Func<Task> task)
+        {
+            lock (_lock)
+            {
+                var now = DateTime.UtcNow;
+                var diff = now - _lastEvent;
+                _lastEvent = now;
+
+                if (diff < _interval)
+                    return Task.CompletedTask;
+            }
+
+            return task();
+        }
+    }
+}
