0fc9c498b2808e9106e16cb08454b9cb7abfa53b
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-10-30 23:13:30 +0100

    Added keyboard state reading

--- LenovoLegionToolkit.WPF/Controls/KeyboardBacklight/Spectrum/SpectrumDevice.xaml.cs
+++ LenovoLegionToolkit.WPF/Controls/KeyboardBacklight/Spectrum/SpectrumDevice.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace LenovoLegionToolkit.WPF.Controls.KeyboardBacklight.Spectrum
+﻿using System;
+using System.Linq;
+using System.Windows;
+using LenovoLegionToolkit.WPF.Extensions;
+
+namespace LenovoLegionToolkit.WPF.Controls.KeyboardBacklight.Spectrum
 {
     public partial class SpectrumDevice
     {
+        public SpectrumKeyboardButton[] Buttons { get; private set; } = Array.Empty<SpectrumKeyboardButton>();
+
         public SpectrumDevice()
         {
             InitializeComponent();
+
+            Loaded += SpectrumDevice_Loaded;
+        }
+
+        private void SpectrumDevice_Loaded(object sender, RoutedEventArgs args)
+        {
+            Buttons = this.GetChildrenOfType<SpectrumKeyboardButton>()
+                .Where(b => b.Visibility == Visibility.Visible)
+                .ToArray();
         }
     }
 }
---
+++ LenovoLegionToolkit.WPF/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace LenovoLegionToolkit.WPF.Extensions
+{
+    public static class DependencyObjectExtensions
+    {
+        public static IEnumerable<T> GetChildrenOfType<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                if (child is T value)
+                {
+                    yield return value;
+                }
+                else
+                {
+                    foreach (var sub in GetChildrenOfType<T>(child))
+                        yield return sub;
+                }
+            }
+        }
+    }
+}
