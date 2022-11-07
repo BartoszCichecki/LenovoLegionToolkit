f3fb010d5bf90c472a1f9bf42f8df4cefd004249
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-07 22:13:41 +0100

    Correct refreshing state on L7 2022

---
+++ LenovoLegionToolkit.Lib/Listeners/ThermalModeListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Management;
+using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.Features;
+
+namespace LenovoLegionToolkit.Lib.Listeners
+{
+    public class ThermalModeListener : AbstractWMIListener<ThermalModeState>
+    {
+        private readonly PowerModeFeature _powerModeFeature;
+        private readonly PowerModeListener _powerModeListener;
+
+        public ThermalModeListener(PowerModeFeature powerModeFeature, PowerModeListener powerModeListener)
+            : base("ROOT\\WMI", "LENOVO_GAMEZONE_THERMAL_MODE_EVENT")
+        {
+            _powerModeFeature = powerModeFeature;
+            _powerModeListener = powerModeListener;
+        }
+
+        protected override ThermalModeState GetValue(PropertyDataCollection properties) => ThermalModeState.IrrelevantAndBuggy;
+
+        protected override async Task OnChangedAsync(ThermalModeState _)
+        {
+            var state = await _powerModeFeature.GetStateAsync();
+            await _powerModeListener.NotifyAsync(state);
+        }
+    }
+}
