13e5ff5ea990efb4d09d66a1e79188b57a39d26a
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-08 22:16:16 +0100

    Added settings data structures

--- LenovoLegionToolkit.Lib/Settings/SpectrumKeyboardSettings.cs
+++ LenovoLegionToolkit.Lib/Settings/SpectrumKeyboardSettings.cs
@@ -21,51 +21,27 @@ namespace LenovoLegionToolkit.Lib.Settings
                 {
                     {
                         SpectrumKeyboardProfile.One,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always,
-                            SpectrumKeyboardSpeed.None,
-                            SpectrumKeyboardDirection.None,
-                            Array.Empty<RGBColor>(),
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always, SpectrumKeyboardSpeed.None, SpectrumKeyboardDirection.None, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
                     },
                     {
                         SpectrumKeyboardProfile.Two,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always,
-                            SpectrumKeyboardSpeed.None,
-                            SpectrumKeyboardDirection.None,
-                            new RGBColor[] { new(255, 0, 0) },
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always, SpectrumKeyboardSpeed.None, SpectrumKeyboardDirection.None, new RGBColor[] { new(255, 0, 0) }, Array.Empty<ushort>()))
                     },
                     {
                         SpectrumKeyboardProfile.Three,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always,
-                            SpectrumKeyboardSpeed.None,
-                            SpectrumKeyboardDirection.None,
-                            new RGBColor[] { new(0, 255, 0) },
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always, SpectrumKeyboardSpeed.None, SpectrumKeyboardDirection.None, new RGBColor[] { new(0, 255, 0) }, Array.Empty<ushort>()))
                     },
                     {
                         SpectrumKeyboardProfile.Four,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always,
-                            SpectrumKeyboardSpeed.None,
-                            SpectrumKeyboardDirection.None,
-                            new RGBColor[] { new(0, 0, 255) },
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.Always, SpectrumKeyboardSpeed.None, SpectrumKeyboardDirection.None, new RGBColor[] { new(0, 0, 255) }, Array.Empty<ushort>()))
                     },
                     {
                         SpectrumKeyboardProfile.Five,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.RainbowWave,
-                            SpectrumKeyboardSpeed.Speed2,
-                            SpectrumKeyboardDirection.BottomToTop,
-                            Array.Empty<RGBColor>(),
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.RainbowWave, SpectrumKeyboardSpeed.Speed2, SpectrumKeyboardDirection.BottomToTop, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
                     },
                     {
                         SpectrumKeyboardProfile.Six,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.RainbowScrew,
-                            SpectrumKeyboardSpeed.Speed2,
-                            SpectrumKeyboardDirection.Clockwise,
-                            Array.Empty<RGBColor>(),
-                            new ushort[] { 0x65 }))
+                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardEffect.RainbowScrew, SpectrumKeyboardSpeed.Speed2, SpectrumKeyboardDirection.Clockwise, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
                     }
                 }
             }
