49011e78fac3fba8a551713c2178ffadcd187af9
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-08 23:22:48 +0100

    Settings serialization fix

--- LenovoLegionToolkit.Lib/Settings/SpectrumKeyboardSettings.cs
+++ LenovoLegionToolkit.Lib/Settings/SpectrumKeyboardSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LenovoLegionToolkit.Lib.Settings
 {
@@ -6,7 +7,7 @@ namespace LenovoLegionToolkit.Lib.Settings
     {
         public class SpectrumKeyboardSettingsStore
         {
-            public KeyboardLayout KeyboardLayout { get; set; } = KeyboardLayout.Ansi;
+            public KeyboardLayout KeyboardLayout { get; set; }
             public SpectrumKeyboardBacklightState State { get; set; }
         }
 
@@ -15,36 +16,32 @@ namespace LenovoLegionToolkit.Lib.Settings
         public override SpectrumKeyboardSettingsStore Default => new()
         {
             KeyboardLayout = KeyboardLayout.Ansi,
-            State = new()
-            {
-                Profiles =
+            State = new(new Dictionary<SpectrumKeyboardBacklightProfile, SpectrumKeyboardBacklightProfileDescription> {
                 {
-                    {
-                        SpectrumKeyboardBacklightProfile.One,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(255, 255, 255) }, Array.Empty<ushort>()))
-                    },
-                    {
-                        SpectrumKeyboardBacklightProfile.Two,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(255, 0, 0) }, Array.Empty<ushort>()))
-                    },
-                    {
-                        SpectrumKeyboardBacklightProfile.Three,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(0, 255, 0) }, Array.Empty<ushort>()))
-                    },
-                    {
-                        SpectrumKeyboardBacklightProfile.Four,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(0, 0, 255) }, Array.Empty<ushort>()))
-                    },
-                    {
-                        SpectrumKeyboardBacklightProfile.Five,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.RainbowWave, SpectrumKeyboardBacklightSpeed.Speed2, SpectrumKeyboardBacklightDirection.BottomToTop, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
-                    },
-                    {
-                        SpectrumKeyboardBacklightProfile.Six,
-                        new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.RainbowScrew, SpectrumKeyboardBacklightSpeed.Speed2, SpectrumKeyboardBacklightDirection.Clockwise, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
-                    }
+                    SpectrumKeyboardBacklightProfile.One,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
+                },
+                {
+                    SpectrumKeyboardBacklightProfile.Two,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(255, 0, 0) }, Array.Empty<ushort>()))
+                },
+                {
+                    SpectrumKeyboardBacklightProfile.Three,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(0, 255, 0) }, Array.Empty<ushort>()))
+                },
+                {
+                    SpectrumKeyboardBacklightProfile.Four,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.Always, SpectrumKeyboardBacklightSpeed.None, SpectrumKeyboardBacklightDirection.None, new RGBColor[] { new(0, 0, 255) }, Array.Empty<ushort>()))
+                },
+                {
+                    SpectrumKeyboardBacklightProfile.Five,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.RainbowWave, SpectrumKeyboardBacklightSpeed.Speed2, SpectrumKeyboardBacklightDirection.BottomToTop, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
+                },
+                {
+                    SpectrumKeyboardBacklightProfile.Six,
+                    new(new SpectrumKeyboardBacklightEffect(SpectrumKeyboardBacklightEffectType.RainbowScrew, SpectrumKeyboardBacklightSpeed.Speed2, SpectrumKeyboardBacklightDirection.Clockwise, Array.Empty<RGBColor>(), Array.Empty<ushort>()))
                 }
-            }
+            })
         };
     }
 }
