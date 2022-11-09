2f146b2941ad38e47a70f93ff9b9503de61ab46e
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-10 00:43:21 +0100

    Fixed effect serialization

--- LenovoLegionToolkit.Lib/Native.cs
+++ LenovoLegionToolkit.Lib/Native.cs
@@ -1,7 +1,12 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO;
+using System.Runtime.InteropServices;
 
 namespace LenovoLegionToolkit.Lib
 {
+    internal interface ICustomBytesSerializable
+    {
+        byte[] ToBytes();
+    }
 
     #region Battery
 
@@ -64,7 +69,7 @@ namespace LenovoLegionToolkit.Lib
 
     internal enum LENOVO_SPECTRUM_EFFECT_TYPE : byte
     {
-        ScreenRainbow = 1,
+        ScrewRainbow = 1,
         RainbowWave = 2,
         ColorChange = 3,
         ColorPulse = 4,
@@ -88,24 +93,41 @@ namespace LenovoLegionToolkit.Lib
 
     internal enum LENOVO_SPECTRUM_SPEED : byte
     {
+        None = 0,
         Speed1 = 1,
         Speed2 = 2,
         Speed3 = 3
     }
 
+    internal enum LENOVO_SPECTRUM_CLOCKWISE_DIRECTION : byte
+    {
+        None = 0,
+        Clockwise = 1,
+        CounterClockwise = 2
+    }
+
     internal enum LENOVO_SPECTRUM_DIRECTION : byte
     {
         None = 0,
-        LTR = 1,
-        RTL = 2
+        BottomToTop = 1,
+        TopToBottom = 2,
+        RightToLeft = 3,
+        LeftToRight = 4
     }
 
     [StructLayout(LayoutKind.Sequential)]
     internal struct LENOVO_SPECTRUM_COLOR
     {
-        public byte Red;
-        public byte Green;
-        public byte Blue;
+        public byte R;
+        public byte G;
+        public byte B;
+
+        public LENOVO_SPECTRUM_COLOR(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -120,10 +142,10 @@ namespace LenovoLegionToolkit.Lib
     {
         public byte Head = 7;
         public LENOVO_SPECTRUM_OPERATION_TYPE Type;
-        public byte Size; // %256
+        public byte Size;
         public byte Tail = 3;
 
-        public LENOVO_SPECTRUM_HEADER(LENOVO_SPECTRUM_OPERATION_TYPE type, int size) : this()
+        public LENOVO_SPECTRUM_HEADER(LENOVO_SPECTRUM_OPERATION_TYPE type, int size)
         {
             Type = type;
             Size = (byte)(size % 256);
@@ -133,34 +155,36 @@ namespace LenovoLegionToolkit.Lib
     [StructLayout(LayoutKind.Sequential)]
     internal struct LENOVO_SPECTRUM_EFFECT_HEADER
     {
-        byte Head = 0x6;
-        byte Unknown1 = 0x1;
-        LENOVO_SPECTRUM_EFFECT_TYPE EffectType;
-        byte Unknown2 = 0x2;
-        LENOVO_SPECTRUM_SPEED Speed;
-        byte Unknown3 = 0x3;
-        byte Unknown4 = 0x0;
-        byte Unknown5 = 0x4;
-        LENOVO_SPECTRUM_DIRECTION Direction;
-        byte Unknown6 = 0x5;
-        LENOVO_SPECTRUM_COLOR_MODE ColorMode;
-        byte Unknown7 = 0x6;
-        byte Tail = 0x0;
+        public byte Head = 0x6;
+        public byte Unknown1 = 0x1;
+        public LENOVO_SPECTRUM_EFFECT_TYPE EffectType;
+        public byte Unknown2 = 0x2;
+        public LENOVO_SPECTRUM_SPEED Speed;
+        public byte Unknown3 = 0x3;
+        public LENOVO_SPECTRUM_CLOCKWISE_DIRECTION ClockwiseDirection;
+        public byte Unknown5 = 0x4;
+        public LENOVO_SPECTRUM_DIRECTION Direction;
+        public byte Unknown6 = 0x5;
+        public LENOVO_SPECTRUM_COLOR_MODE ColorMode;
+        public byte Unknown7 = 0x6;
+        public byte Tail = 0x0;
 
         public LENOVO_SPECTRUM_EFFECT_HEADER(
             LENOVO_SPECTRUM_EFFECT_TYPE effectType,
             LENOVO_SPECTRUM_SPEED speed,
             LENOVO_SPECTRUM_DIRECTION direction,
+            LENOVO_SPECTRUM_CLOCKWISE_DIRECTION clockwiseDirection,
             LENOVO_SPECTRUM_COLOR_MODE colorMode)
         {
             EffectType = effectType;
             Speed = speed;
             Direction = direction;
+            ClockwiseDirection = clockwiseDirection;
             ColorMode = colorMode;
         }
     }
 
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     internal struct LENOVO_SPECTRUM_EFFECT
     {
         public byte EffectNo;
@@ -169,6 +193,16 @@ namespace LenovoLegionToolkit.Lib
         public LENOVO_SPECTRUM_COLOR[] Colors;
         public byte NumberOfKeys;
         public ushort[] Keys;
+
+        public LENOVO_SPECTRUM_EFFECT(LENOVO_SPECTRUM_EFFECT_HEADER effectHeader, int effectNo, LENOVO_SPECTRUM_COLOR[] colors, ushort[] keys)
+        {
+            EffectHeader = effectHeader;
+            EffectNo = (byte)effectNo;
+            NumberOfColors = (byte)colors.Length;
+            Colors = colors;
+            NumberOfKeys = (byte)keys.Length;
+            Keys = keys;
+        }
     }
 
     [StructLayout(LayoutKind.Sequential, Size = 960)]
@@ -239,8 +273,8 @@ namespace LenovoLegionToolkit.Lib
         }
     }
 
-    [StructLayout(LayoutKind.Sequential, Size = 960)]
-    internal struct LENOVO_SPECTRUM_SET_EFFECTS
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct LENOVO_SPECTRUM_SET_EFFECTS : ICustomBytesSerializable
     {
         public LENOVO_SPECTRUM_HEADER Header;
         public byte Profile;
@@ -248,11 +282,65 @@ namespace LenovoLegionToolkit.Lib
         public byte Unknown2 = 1;
         public LENOVO_SPECTRUM_EFFECT[] Effects;
 
-        public LENOVO_SPECTRUM_SET_EFFECTS(LENOVO_SPECTRUM_HEADER header, byte profile, LENOVO_SPECTRUM_EFFECT[] effects) : this()
+        public LENOVO_SPECTRUM_SET_EFFECTS(LENOVO_SPECTRUM_HEADER header, byte profile, LENOVO_SPECTRUM_EFFECT[] effects)
         {
             Header = header;
             Profile = profile;
             Effects = effects;
+        }
+
+        public byte[] ToBytes()
+        {
+            using var ms = new MemoryStream(new byte[960]);
+            using var bf = new BinaryWriter(ms);
+
+            bf.Write(Header.Head);
+            bf.Write((byte)Header.Type);
+            bf.Write(Header.Size);
+            bf.Write(Header.Tail);
+
+            bf.Write(Profile);
+            bf.Write(Unknown1);
+            bf.Write(Unknown2);
+
+            foreach (var effect in Effects)
+            {
+                bf.Write(effect.EffectNo);
+
+                bf.Write(effect.EffectHeader.Head);
+                bf.Write(effect.EffectHeader.Unknown1);
+                bf.Write((byte)effect.EffectHeader.EffectType);
+                bf.Write(effect.EffectHeader.Unknown2);
+                bf.Write((byte)effect.EffectHeader.Speed);
+                bf.Write(effect.EffectHeader.Unknown3);
+                bf.Write((byte)effect.EffectHeader.ClockwiseDirection);
+                bf.Write(effect.EffectHeader.Unknown5);
+                bf.Write((byte)effect.EffectHeader.Direction);
+                bf.Write(effect.EffectHeader.Unknown6);
+                bf.Write((byte)effect.EffectHeader.ColorMode);
+                bf.Write(effect.EffectHeader.Unknown7);
+                bf.Write(effect.EffectHeader.Tail);
+
+                bf.Write(effect.NumberOfColors);
+                foreach (var color in effect.Colors)
+                {
+                    bf.Write(color.R);
+                    bf.Write(color.G);
+                    bf.Write(color.B);
+                }
+
+                bf.Write(effect.NumberOfKeys);
+                foreach (var key in effect.Keys)
+                {
+                    bf.Write(key);
+                }
+            }
+
+            var position = ms.Position;
+            bf.Seek(2, SeekOrigin.Begin);
+            bf.Write((byte)(position % 255));
+
+            return ms.ToArray();
         }
     }
 
