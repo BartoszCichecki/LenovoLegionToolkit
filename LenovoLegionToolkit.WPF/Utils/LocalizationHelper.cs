cbfe738d9da1cf2ef88ee4a4e9d45bb5a359426a
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-15 13:17:11 +0100

    Enabled German, Italian and French localizations.

--- LenovoLegionToolkit.WPF/Utils/LocalizationHelper.cs
+++ LenovoLegionToolkit.WPF/Utils/LocalizationHelper.cs
@@ -15,8 +15,11 @@ namespace LenovoLegionToolkit.WPF.Utils
 
         public static readonly CultureInfo[] Languages = {
             new("cs"),
+            new("de"),
             new("en"),
             new("es"),
+            new("fr"),
+            new("it"),
             new("nl-nl"),
             new("pt"),
             new("pt-br"),
