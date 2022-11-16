00888b8fc3212fc0baef1bdfb3d3130dccc1cb37
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-16 19:59:06 +0100

    Formatting

--- LenovoLegionToolkit.Lib/System/SystemTheme.cs
+++ LenovoLegionToolkit.Lib/System/SystemTheme.cs
@@ -6,25 +6,24 @@ namespace LenovoLegionToolkit.Lib.System
 {
     public static class SystemTheme
     {
-        private const string RegistryHive = "HKEY_CURRENT_USER";
+        private const string REGISTRY_HIVE = "HKEY_CURRENT_USER";
 
-        private const string PersonalizeRegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
-        private const string AppsUseLightThemeRegistryKey = "AppsUseLightTheme";
+        private const string PERSONALIZE_REGISTRY_PATH = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+        private const string APPS_USE_LIGHT_THEME_REGISTRY_KEY = "AppsUseLightTheme";
 
-        private const string DwmRegistryPath = @"Software\Microsoft\Windows\DWM";
-        private const string DwmColorizationColorRegistryKey = "ColorizationColor";
+        private const string DWM_REGISTRY_PATH = @"Software\Microsoft\Windows\DWM";
+        private const string DWM_COLORIZATION_COLOR_REGISTRY_KEY = "ColorizationColor";
 
         public static bool GetDarkMode()
         {
-            var registryValue = Registry.Read(RegistryHive, PersonalizeRegistryPath, AppsUseLightThemeRegistryKey, -1);
+            var registryValue = Registry.Read(REGISTRY_HIVE, PERSONALIZE_REGISTRY_PATH, APPS_USE_LIGHT_THEME_REGISTRY_KEY, -1);
             if (registryValue == -1)
-                throw new InvalidOperationException($"Couldn't read the {AppsUseLightThemeRegistryKey} setting.");
+                throw new InvalidOperationException($"Couldn't read the {APPS_USE_LIGHT_THEME_REGISTRY_KEY} setting.");
 
             return registryValue == 0;
         }
 
-        public static RGBColor GetThemeMatchedAccentColor() =>
-            GetUxThemeImmersiveColor(GetDarkMode() ? "SystemAccentLight2" : "SystemAccentDark1");
+        public static RGBColor GetThemeMatchedAccentColor() => GetUxThemeImmersiveColor(GetDarkMode() ? "SystemAccentLight2" : "SystemAccentDark1");
 
         public static RGBColor GetUxThemeImmersiveColor(string name)
         {
@@ -44,10 +43,10 @@ namespace LenovoLegionToolkit.Lib.System
 
         internal static RGBColor GetAccentColorReg()
         {
-            var registryValue = Registry.Read(RegistryHive, DwmRegistryPath, DwmColorizationColorRegistryKey, -1);
+            var registryValue = Registry.Read(REGISTRY_HIVE, DWM_REGISTRY_PATH, DWM_COLORIZATION_COLOR_REGISTRY_KEY, -1);
 
             if (registryValue == -1)
-                throw new InvalidOperationException($"Couldn't read the {DwmColorizationColorRegistryKey} setting.");
+                throw new InvalidOperationException($"Couldn't read the {DWM_COLORIZATION_COLOR_REGISTRY_KEY} setting.");
 
             var registryValueBytes = BitConverter.GetBytes(registryValue);
 
@@ -55,19 +54,22 @@ namespace LenovoLegionToolkit.Lib.System
         }
 
         internal static IDisposable GetDarkModeListener(Action callback) =>
-            Registry.Listen(RegistryHive, PersonalizeRegistryPath, AppsUseLightThemeRegistryKey, callback);
+            Registry.Listen(REGISTRY_HIVE, PERSONALIZE_REGISTRY_PATH, APPS_USE_LIGHT_THEME_REGISTRY_KEY, callback);
 
         internal static IDisposable GetAccentColorListener(Action callback) =>
-             Registry.Listen(RegistryHive, DwmRegistryPath, DwmColorizationColorRegistryKey, callback);
+             Registry.Listen(REGISTRY_HIVE, DWM_REGISTRY_PATH, DWM_COLORIZATION_COLOR_REGISTRY_KEY, callback);
+
+        #region Native
 
         [DllImport("uxtheme.dll", EntryPoint = "#95")]
-        private static extern uint GetImmersiveColorFromColorSetEx(uint immersiveColorSet, uint immersiveColorType,
-            bool ignoreHighContrast, uint highContrastCacheMode);
+        private static extern uint GetImmersiveColorFromColorSetEx(uint immersiveColorSet, uint immersiveColorType, bool ignoreHighContrast, uint highContrastCacheMode);
 
         [DllImport("uxtheme.dll", EntryPoint = "#96", CharSet = CharSet.Auto)]
         private static extern uint GetImmersiveColorTypeFromName(string name);
 
         [DllImport("uxtheme.dll", EntryPoint = "#98")]
         private static extern uint GetImmersiveUserColorSetPreference(bool forceCheckRegistry, bool skipCheckOnFail);
+
+        #endregion
     }
 }
