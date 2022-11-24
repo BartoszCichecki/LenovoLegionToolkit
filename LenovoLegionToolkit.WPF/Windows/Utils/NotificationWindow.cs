af6d060c5bfa7260877a9532d8b176dabe73ffbb
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-25 00:49:23 +0100

    Fix working area calculation to respond nicely to scale change.

--- LenovoLegionToolkit.WPF/Windows/Utils/NotificationWindow.cs
+++ LenovoLegionToolkit.WPF/Windows/Utils/NotificationWindow.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using LenovoLegionToolkit.Lib;
+using Windows.Win32;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.UI.HiDpi;
 using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
-using Screen = System.Windows.Forms.Screen;
+using Point = System.Drawing.Point;
+
+#pragma warning disable CA1416 // Validate platform compatibility
 
 namespace LenovoLegionToolkit.WPF.Windows.Utils
 {
@@ -46,7 +52,7 @@ namespace LenovoLegionToolkit.WPF.Windows.Utils
             InitializeStyle();
             InitializeContent(symbol, overlaySymbol, symbolTransform, text);
 
-            SourceInitialized += (_, _) => InitializePosition(position, GetRealDesktopWorkingArea());
+            SourceInitialized += (_, _) => InitializePosition(position, GetPrimaryDesktopWorkingArea());
             MouseDown += (_, _) => Close();
         }
 
@@ -59,12 +65,22 @@ namespace LenovoLegionToolkit.WPF.Windows.Utils
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
-        private Rect GetRealDesktopWorkingArea()
+        private Rect GetPrimaryDesktopWorkingArea()
         {
-            var wa = Screen.FromHandle(HwndSource.Handle).WorkingArea;
-            var scale = HwndSource.CompositionTarget?.TransformToDevice.M11 ?? 1;
-            var multiplier = 96 / (96 * scale);
-            return new Rect(wa.X, wa.Y, wa.Width * multiplier, wa.Height * multiplier);
+            var screen = PInvoke.MonitorFromPoint(Point.Empty, MONITOR_FROM_FLAGS.MONITOR_DEFAULTTOPRIMARY);
+
+            var monitorInfo = new MONITORINFO { cbSize = (uint)Marshal.SizeOf<MONITORINFO>() };
+            if (!PInvoke.GetMonitorInfo(screen, ref monitorInfo))
+                return SystemParameters.WorkArea;
+
+            if (!PInvoke.GetDpiForMonitor(screen, MONITOR_DPI_TYPE.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY).Succeeded)
+                return SystemParameters.WorkArea;
+
+            var workArea = monitorInfo.rcWork;
+            var multiplierX = 96d / dpiX;
+            var multiplierY = 96d / dpiY;
+
+            return new Rect(workArea.X, workArea.Y, workArea.Width * multiplierX, workArea.Height * multiplierY);
         }
 
         private void InitializeStyle()
