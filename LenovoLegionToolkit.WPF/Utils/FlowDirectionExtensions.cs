070d4eae3c36860e2c8550291f35472a374d416c
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-26 18:52:24 +0100

    RTL support (#412)
    * Enabled RTL layout.
    
    * Disable ar for now until it's ready.

---
+++ LenovoLegionToolkit.WPF/Utils/FlowDirectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using LenovoLegionToolkit.WPF.Resources;
+
+namespace LenovoLegionToolkit.WPF.Utils
+{
+    public static class FlowDirectionExtensions
+    {
+        public static FlowDirection Direction => Resource.Culture.TextInfo.IsRightToLeft
+            ? FlowDirection.RightToLeft
+            : FlowDirection.LeftToRight;
+    }
+}
