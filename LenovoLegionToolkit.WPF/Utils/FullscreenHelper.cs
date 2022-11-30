88a2ecf84d989d9757b6064eecd2a25be4a64dc7
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2022-11-30 21:49:45 +0100

    Fix notifications not showing when desktop is in focus

--- LenovoLegionToolkit.WPF/Utils/FullscreenHelper.cs
+++ LenovoLegionToolkit.WPF/Utils/FullscreenHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using LenovoLegionToolkit.Lib.Utils;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 
@@ -8,19 +11,44 @@ public static class FullscreenHelper
 {
     public static bool IsAnyApplicationFullscreen()
     {
-        var hwndDesktop = PInvoke.GetDesktopWindow();
-        var hwndShell = PInvoke.GetShellWindow();
+        try
+        {
+            var desktopWindowHandle = PInvoke.GetDesktopWindow();
+            var shellWindowHandle = PInvoke.GetShellWindow();
 
-        var hwndForeground = PInvoke.GetForegroundWindow();
-        if (hwndForeground == HWND.Null || hwndForeground == hwndDesktop || hwndForeground == hwndShell)
+            var foregroundWindowHandle = PInvoke.GetForegroundWindow();
+            if (foregroundWindowHandle == HWND.Null)
+                return false;
+            if (foregroundWindowHandle == desktopWindowHandle)
+                return false;
+            if (foregroundWindowHandle == shellWindowHandle)
+                return false;
+
+            if (!PInvoke.GetWindowRect(foregroundWindowHandle, out var appBounds))
+                return false;
+
+            var screenBounds = Screen.FromHandle(foregroundWindowHandle.Value).Bounds;
+            var coversFullScreen = appBounds.bottom - appBounds.top == screenBounds.Height && appBounds.right - appBounds.left == screenBounds.Width;
+            if (!coversFullScreen)
+                return false;
+
+            unsafe
+            {
+                var processId = 0u;
+                _ = PInvoke.GetWindowThreadProcessId(foregroundWindowHandle, &processId);
+                var process = Process.GetProcessById((int)processId);
+                if (process.ProcessName == "explorer")
+                    return false;
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            if (Log.Instance.IsTraceEnabled)
+                Log.Instance.Trace($"Couldn't check if application is full screen.", ex);
+
             return false;
-
-        var result = PInvoke.GetWindowRect(hwndForeground, out var appBounds);
-        if (!result)
-            return false;
-
-        var screenBounds = Screen.FromHandle(hwndForeground.Value).Bounds;
-
-        return appBounds.bottom - appBounds.top == screenBounds.Height && appBounds.right - appBounds.left == screenBounds.Width;
+        }
     }
 }
