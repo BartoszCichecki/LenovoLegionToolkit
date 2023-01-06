5ad9aaa62a5233836320630f29726bb70c626247
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-01-06 18:57:56 +0100

    Formatting

--- LenovoLegionToolkit.WPF/Controls/Automation/Steps/MicrophoneMuteAutomationStepControl.cs
+++ LenovoLegionToolkit.WPF/Controls/Automation/Steps/MicrophoneMuteAutomationStepControl.cs
@@ -3,15 +3,14 @@ using LenovoLegionToolkit.Lib.Automation.Steps;
 using LenovoLegionToolkit.WPF.Resources;
 using Wpf.Ui.Common;
 
-namespace LenovoLegionToolkit.WPF.Controls.Automation.Steps
+namespace LenovoLegionToolkit.WPF.Controls.Automation.Steps;
+
+public class MicrophoneMuteAutomationStepControl : AbstractComboBoxAutomationStepCardControl<MicrophoneMuteState>
 {
-    public class MicrophoneMuteAutomationStepControl : AbstractComboBoxAutomationStepCardControl<MicrophoneMuteState>
+    public MicrophoneMuteAutomationStepControl(IAutomationStep<MicrophoneMuteState> step) : base(step)
     {
-        public MicrophoneMuteAutomationStepControl(IAutomationStep<MicrophoneMuteState> step) : base(step)
-        {
-            Icon = SymbolRegular.MicOff24;
-            Title = Resource.MicrophoneMuteAutomationStepControl_Title;
-            Subtitle = Resource.MicrophoneMuteAutomationStepControl_Message;
-        }
+        Icon = SymbolRegular.MicOff24;
+        Title = Resource.MicrophoneMuteAutomationStepControl_Title;
+        Subtitle = Resource.MicrophoneMuteAutomationStepControl_Message;
     }
 }
