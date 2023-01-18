4211fc248e95973bb5428575205305d9f22b119a
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-01-18 16:50:53 +0100

    Make IoCContainer more thread safe

--- LenovoLegionToolkit.Lib/IoCContainer.cs
+++ LenovoLegionToolkit.Lib/IoCContainer.cs
@@ -5,34 +5,45 @@ namespace LenovoLegionToolkit.Lib;
 
 public class IoCContainer
 {
+    private static readonly object _lock = new();
+
     private static IContainer? _container;
 
     public static void Initialize(params Module[] modules)
     {
-        if (_container is not null)
-            throw new InvalidOperationException("IoCContainer already initialized");
+        lock (_lock)
+        {
+            if (_container is not null)
+                throw new InvalidOperationException("IoCContainer already initialized");
 
-        var cb = new ContainerBuilder();
+            var cb = new ContainerBuilder();
 
-        foreach (var module in modules)
-            cb.RegisterModule(module);
+            foreach (var module in modules)
+                cb.RegisterModule(module);
 
-        _container = cb.Build();
+            _container = cb.Build();
+        }
     }
 
     public static T Resolve<T>() where T : notnull
     {
-        if (_container is null)
-            throw new InvalidOperationException("IoCContainer must be initialized first");
-        return _container.Resolve<T>();
+        lock (_lock)
+        {
+            if (_container is null)
+                throw new InvalidOperationException($"IoCContainer must be initialized first. [type={nameof(T)}]");
+            return _container.Resolve<T>();
+        }
     }
 
     public static T? TryResolve<T>() where T : class
     {
-        if (_container is null)
-            throw new InvalidOperationException("IoCContainer must be initialized first");
-        _ = _container.TryResolve(out T? value);
-        return value;
+        lock (_lock)
+        {
+            if (_container is null)
+                throw new InvalidOperationException($"IoCContainer must be initialized first [type={nameof(T)}]");
+            _ = _container.TryResolve(out T? value);
+            return value;
+        }
     }
 
 }
