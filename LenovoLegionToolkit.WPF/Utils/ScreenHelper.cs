5dde484f7288a3a9749eb1cc370aec8879b26a4a
Mario Bălănică <mariobalanica02@gmail.com>
2023-02-06 23:11:35 +0200

    Recenter main window location

---
+++ LenovoLegionToolkit.WPF/Utils/ScreenHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using Windows.Win32;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.UI.HiDpi;
+using Point = System.Drawing.Point;
+
+#pragma warning disable CA1416 // Validate platform compatibility
+
+namespace LenovoLegionToolkit.WPF.Utils;
+
+public static class ScreenHelper
+{
+    public static Rect GetPrimaryDesktopWorkingArea()
+    {
+        var screen = PInvoke.MonitorFromPoint(Point.Empty, MONITOR_FROM_FLAGS.MONITOR_DEFAULTTOPRIMARY);
+
+        var monitorInfo = new MONITORINFO { cbSize = (uint)Marshal.SizeOf<MONITORINFO>() };
+        if (!PInvoke.GetMonitorInfo(screen, ref monitorInfo))
+            return SystemParameters.WorkArea;
+
+        if (!PInvoke.GetDpiForMonitor(screen, MONITOR_DPI_TYPE.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY).Succeeded)
+            return SystemParameters.WorkArea;
+
+        var workArea = monitorInfo.rcWork;
+        var multiplierX = 96d / dpiX;
+        var multiplierY = 96d / dpiY;
+
+        return new Rect(workArea.X, workArea.Y, workArea.Width * multiplierX, workArea.Height * multiplierY);
+    }
+}
--- LenovoLegionToolkit.WPF/Windows/Utils/NotificationWindow.cs
+++ LenovoLegionToolkit.WPF/Windows/Utils/NotificationWindow.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using LenovoLegionToolkit.Lib;
 using LenovoLegionToolkit.WPF.Utils;
-using Windows.Win32;
-using Windows.Win32.Graphics.Gdi;
-using Windows.Win32.UI.HiDpi;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
-using Point = System.Drawing.Point;
-
-#pragma warning disable CA1416 // Validate platform compatibility
 
 namespace LenovoLegionToolkit.WPF.Windows.Utils;
 
@@ -67,24 +60,6 @@ public class NotificationWindow : UiWindow
         }, TaskScheduler.FromCurrentSynchronizationContext());
     }
 
-    private Rect GetPrimaryDesktopWorkingArea()
-    {
-        var screen = PInvoke.MonitorFromPoint(Point.Empty, MONITOR_FROM_FLAGS.MONITOR_DEFAULTTOPRIMARY);
-
-        var monitorInfo = new MONITORINFO { cbSize = (uint)Marshal.SizeOf<MONITORINFO>() };
-        if (!PInvoke.GetMonitorInfo(screen, ref monitorInfo))
-            return SystemParameters.WorkArea;
-
-        if (!PInvoke.GetDpiForMonitor(screen, MONITOR_DPI_TYPE.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY).Succeeded)
-            return SystemParameters.WorkArea;
-
-        var workArea = monitorInfo.rcWork;
-        var multiplierX = 96d / dpiX;
-        var multiplierY = 96d / dpiY;
-
-        return new Rect(workArea.X, workArea.Y, workArea.Width * multiplierX, workArea.Height * multiplierY);
-    }
-
     private void InitializeStyle()
     {
         WindowStartupLocation = WindowStartupLocation.Manual;
@@ -103,7 +78,7 @@ public class NotificationWindow : UiWindow
 
     private void InitializePosition(NotificationPosition position)
     {
-        var desktopWorkingArea = GetPrimaryDesktopWorkingArea();
+        var desktopWorkingArea = ScreenHelper.GetPrimaryDesktopWorkingArea();
 
         _mainGrid.Measure(new Size(double.PositiveInfinity, 80));
 
