47a2f2fd8690e8b75c14f2c79dcf3ec4912efe31
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-02-09 23:35:08 +0100

    Fail make on error

--- make.bat
+++ make.bat
@@ -9,7 +9,7 @@ IF "%1"=="" (
 
 SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
 
-dotnet publish LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION%
-dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION%
+dotnet publish LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 
-iscc make_installer.iss /DMyAppVersion=%VERSION%
+iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
