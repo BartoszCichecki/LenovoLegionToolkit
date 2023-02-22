ec0b55b90a04e9eb7fd7432c2d9cdb039293cbe3
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-02-22 22:10:51 +0100

    Clean up unused code.

--- LenovoLegionToolkit.WPF/Windows/Automation/CreateAutomationPipelineWindow.xaml.cs
+++ LenovoLegionToolkit.WPF/Windows/Automation/CreateAutomationPipelineWindow.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using LenovoLegionToolkit.Lib;
 using LenovoLegionToolkit.Lib.Automation.Pipeline.Triggers;
-using LenovoLegionToolkit.Lib.Extensions;
 using LenovoLegionToolkit.WPF.Controls;
 using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
@@ -36,8 +33,6 @@ public partial class CreateAutomationPipelineWindow
     private readonly HashSet<Type> _existingTriggerTypes;
     private readonly Action<IAutomationPipelineTrigger> _createPipeline;
 
-    private bool _multiSelect;
-
     public CreateAutomationPipelineWindow(HashSet<Type> existingTriggerTypes, Action<IAutomationPipelineTrigger> createPipeline)
     {
         _existingTriggerTypes = existingTriggerTypes;
@@ -54,99 +49,27 @@ public partial class CreateAutomationPipelineWindow
             await RefreshAsync();
     }
 
-    private void CreateButton_Click(object sender, RoutedEventArgs e)
-    {
-        var triggers = _content.Children.ToArray()
-            .OfType<CardControl>()
-            .Select(c => c.Header)
-            .OfType<CardHeaderControl>()
-            .Select(c => c.Accessory)
-            .OfType<CheckBox>()
-            .Where(c => c.IsChecked ?? false)
-            .Select(c => c.Tag)
-            .OfType<IAutomationPipelineTrigger>()
-            .ToArray();
-
-        if (triggers.IsEmpty())
-            return;
-
-        // var trigger = triggers.Length == 1 ? triggers.First() : new AndAutomationPipelineTrigger(triggers);
-        var trigger = triggers.First();
-        _createPipeline(trigger);
-
-        Close();
-    }
-
     private void CancelButton_Click(object sender, RoutedEventArgs e) => Close();
 
     private Task RefreshAsync()
     {
         _content.Children.Clear();
 
-        // if (!_multiSelect)
-        //     _content.Children.Add(CreateMultipleSelectCardControl());
-
         foreach (var trigger in _triggers)
             _content.Children.Add(CreateCardControl(trigger));
-
-        _createButton.IsEnabled = false;
-        _createButton.Visibility = _multiSelect ? Visibility.Visible : Visibility.Collapsed;
 
         return Task.CompletedTask;
     }
 
-    // private UIElement CreateMultipleSelectCardControl()
-    // {
-    //     var control = new CardControl
-    //     {
-    //         Icon = SymbolRegular.SquareMultiple24,
-    //         Header = new CardHeaderControl
-    //         {
-    //             Title = "Multiple triggers...",
-    //             Accessory = new SymbolIcon { Symbol = SymbolRegular.ChevronRight24 }
-    //         },
-    //         Margin = new(0, 8, 0, 0),
-    //     };
-    //
-    //     control.Click += async (_, _) =>
-    //     {
-    //         _multiSelect = true;
-    //         await RefreshAsync();
-    //     };
-    //
-    //     return control;
-    // }
-
     private UIElement CreateCardControl(IAutomationPipelineTrigger trigger)
     {
-        UIElement accessory;
-
-        if (_multiSelect)
-        {
-            var checkbox = new CheckBox
-            {
-                Tag = trigger,
-                HorizontalAlignment = HorizontalAlignment.Right
-            };
-            checkbox.Click += (_, e) =>
-            {
-                RefreshCreateButton();
-                e.Handled = true;
-            };
-            accessory = checkbox;
-        }
-        else
-        {
-            accessory = new SymbolIcon { Symbol = SymbolRegular.ChevronRight24 };
-        }
-
         var control = new CardControl
         {
             Icon = IconForTrigger(trigger),
             Header = new CardHeaderControl
             {
                 Title = trigger.DisplayName,
-                Accessory = accessory
+                Accessory = new SymbolIcon { Symbol = SymbolRegular.ChevronRight24 }
             },
             Margin = new(0, 8, 0, 0),
         };
@@ -156,42 +79,11 @@ public partial class CreateAutomationPipelineWindow
 
         control.Click += (_, _) =>
         {
-            if (_multiSelect)
-            {
-                if (accessory is not CheckBox checkbox)
-                    return;
-
-                var isChecked = checkbox.IsChecked ?? false;
-                checkbox.IsChecked = !isChecked;
-                RefreshCreateButton();
-            }
-            else
-            {
-                _createPipeline(trigger);
-                Close();
-            }
+            _createPipeline(trigger);
+            Close();
         };
 
         return control;
-    }
-
-    private void RefreshCreateButton()
-    {
-        if (!_multiSelect)
-        {
-            _createButton.IsEnabled = false;
-            return;
-        }
-
-        var anyChecked = _content.Children.ToArray()
-            .OfType<CardControl>()
-            .Select(c => c.Header)
-            .OfType<CardHeaderControl>()
-            .Select(c => c.Accessory)
-            .OfType<CheckBox>()
-            .Any(c => c.IsChecked ?? false);
-
-        _createButton.IsEnabled = anyChecked;
     }
 
     private static SymbolRegular IconForTrigger(IAutomationPipelineTrigger trigger) => trigger switch
