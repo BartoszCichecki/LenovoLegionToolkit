749b8877424ef751d5d83caaed4d99d573276f7f
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-03-06 22:10:39 +0100

    Registry methods clean up

--- LenovoLegionToolkit.Lib.Automation/GameDetection/GameDetector.cs
+++ LenovoLegionToolkit.Lib.Automation/GameDetection/GameDetector.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
-using LenovoLegionToolkit.Lib.Extensions;
+using LenovoLegionToolkit.Lib.System;
 using LenovoLegionToolkit.Lib.Utils;
-using Microsoft.Win32;
-using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.System.Registry;
 
 namespace LenovoLegionToolkit.Lib.Automation.GameDetection;
 
 internal class GameDetector
 {
+    private const string GAME_CONFIG_STORE_HIVE = "HKEY_CURRENT_USER";
     private const string GAME_CONFIG_STORE_PATH = @"System\GameConfigStore\Children";
     private const string MATCHED_EXE_FULL_PATH_KEY_NAME = "MatchedExeFullPath";
 
@@ -28,52 +24,54 @@ internal class GameDetector
 
     public event EventHandler<GameDetectedEventArgs>? GamesDetected;
 
-    private CancellationTokenSource? _cancellationTokenSource;
-    private Task? _listenTask;
+    private IAsyncDisposable? _listener;
 
     public Task StartAsync()
     {
-        if (_listenTask is not null)
+        if (_listener is not null)
             return Task.CompletedTask;
 
-        _cancellationTokenSource = new();
-        _listenTask = Task.Run(() => Handler(_cancellationTokenSource.Token));
+        var lastPaths = GetDetectedGamePaths();
+
+        _listener = Registry.ObserveKey(GAME_CONFIG_STORE_HIVE, GAME_CONFIG_STORE_PATH, true, () =>
+        {
+            if (Log.Instance.IsTraceEnabled)
+                Log.Instance.Trace($"Registry changed...");
+
+            var newPaths = GetDetectedGamePaths();
+
+            if (!newPaths.SetEquals(lastPaths))
+            {
+                if (Log.Instance.IsTraceEnabled)
+                    Log.Instance.Trace($"Detected games changed.");
+
+                GamesDetected?.Invoke(this, new(newPaths));
+            }
+
+            lastPaths = newPaths;
+        });
 
         return Task.CompletedTask;
     }
 
     public async Task StopAsync()
     {
-        _cancellationTokenSource?.Cancel();
-        _cancellationTokenSource = null;
-        if (_listenTask != null)
-            await _listenTask;
-        _listenTask = null;
+        if (_listener != null)
+            await _listener.DisposeAsync();
+        _listener = null;
     }
 
     public static HashSet<ProcessInfo> GetDetectedGamePaths()
     {
         if (Log.Instance.IsTraceEnabled)
-            Log.Instance.Trace($"Finding detected games...");
+            Log.Instance.Trace($"Detecting games...");
 
         var result = new HashSet<ProcessInfo>();
 
-        var key = Registry.CurrentUser.OpenSubKey(GAME_CONFIG_STORE_PATH);
-
-        if (key is null)
+        foreach (var subKey in Registry.GetSubKeys(GAME_CONFIG_STORE_HIVE, GAME_CONFIG_STORE_PATH))
         {
-            if (Log.Instance.IsTraceEnabled)
-                Log.Instance.Trace($"Couldn't open {GAME_CONFIG_STORE_PATH} key.");
-
-            return new();
-        }
-
-        var subKeyNames = key.GetSubKeyNames();
-
-        foreach (var subKeyName in subKeyNames)
-        {
-            var exePath = key?.OpenSubKey(subKeyName)?.GetValue(MATCHED_EXE_FULL_PATH_KEY_NAME)?.ToString();
-            if (exePath is null)
+            var exePath = Registry.GetValue(GAME_CONFIG_STORE_HIVE, subKey, MATCHED_EXE_FULL_PATH_KEY_NAME, string.Empty);
+            if (string.IsNullOrEmpty(exePath))
                 continue;
 
             result.Add(ProcessInfo.FromPath(exePath));
@@ -87,52 +85,5 @@ internal class GameDetector
         }
 
         return result;
-    }
-
-    private void Handler(CancellationToken token)
-    {
-        try
-        {
-            var lastPaths = GetDetectedGamePaths();
-
-            var key = Registry.CurrentUser.OpenSubKey(GAME_CONFIG_STORE_PATH);
-            if (key is null)
-                throw new InvalidOperationException($"Key {GAME_CONFIG_STORE_PATH} could not be opened.");
-
-            var resetEvent = new ManualResetEvent(false);
-
-            while (true)
-            {
-                var regNotifyChangeKeyValueResult = PInvoke.RegNotifyChangeKeyValue(key.Handle,
-                    true,
-                    REG_NOTIFY_FILTER.REG_NOTIFY_CHANGE_LAST_SET | REG_NOTIFY_FILTER.REG_NOTIFY_THREAD_AGNOSTIC,
-                    resetEvent.SafeWaitHandle,
-                    true);
-                if (regNotifyChangeKeyValueResult != WIN32_ERROR.NO_ERROR)
-                    PInvokeExtensions.ThrowIfWin32Error("RegNotifyChangeKeyValue");
-
-                WaitHandle.WaitAny(new[] { resetEvent, token.WaitHandle });
-                token.ThrowIfCancellationRequested();
-
-                if (Log.Instance.IsTraceEnabled)
-                    Log.Instance.Trace($"Games changed.");
-
-                var newPaths = GetDetectedGamePaths();
-
-                if (!newPaths.SetEquals(lastPaths))
-                    GamesDetected?.Invoke(this, new(newPaths));
-
-                lastPaths = newPaths;
-
-                resetEvent.Reset();
-            }
-        }
-        catch (OperationCanceledException) { }
-        catch (ThreadAbortException) { }
-        catch (Exception ex)
-        {
-            if (Log.Instance.IsTraceEnabled)
-                Log.Instance.Trace($"Unknown error.", ex);
-        }
     }
 }
---
+++ LenovoLegionToolkit.Lib/Utils/LambdaDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LenovoLegionToolkit.Lib.Utils;
+
+public class LambdaDisposable : IDisposable
+{
+    private readonly Action _action;
+
+    public LambdaDisposable(Action action) => _action = action;
+
+    public void Dispose() => _action();
+}
