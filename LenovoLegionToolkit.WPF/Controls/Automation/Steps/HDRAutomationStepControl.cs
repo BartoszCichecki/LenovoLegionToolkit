bba31cb0a9e67a5054a2e3fe912b64a9afe04a3a
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-03-19 01:17:17 +0100

    fix controls not re-appearing after monitor connected event

--- LenovoLegionToolkit.WPF/Controls/Automation/Steps/HDRAutomationStepControl.cs
+++ LenovoLegionToolkit.WPF/Controls/Automation/Steps/HDRAutomationStepControl.cs
@@ -22,7 +22,7 @@ public class HDRAutomationStepControl : AbstractComboBoxAutomationStepCardContro
 
     private void Listener_Changed(object? sender, EventArgs e) => Dispatcher.Invoke(async () =>
     {
-        if (IsLoaded && IsVisible)
+        if (IsLoaded)
             await RefreshAsync();
     });
 }
