8217c77043fdffb0ddf20ba6a4f81f15d7436ab4
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-06-26 17:15:51 +0200

    Hide white keyboard backlight control if RGB or Spectrum is detected. (#821)

--- LenovoLegionToolkit.Lib/Features/PanelLogoBacklightFeature.cs
+++ LenovoLegionToolkit.Lib/Features/PanelLogoBacklightFeature.cs
@@ -8,44 +8,43 @@ public class PanelLogoBacklightFeature : IFeature<PanelLogoBacklightState>
     private readonly PanelLogoLenovoLightingBacklightFeature _lenovoLightingFeature;
     private readonly PanelLogoSpectrumBacklightFeature _spectrumFeature;
 
-    private IFeature<PanelLogoBacklightState>? _feature;
+    private readonly Lazy<Task<IFeature<PanelLogoBacklightState>?>> _lazyAsyncFeature;
 
     public PanelLogoBacklightFeature(PanelLogoLenovoLightingBacklightFeature lenovoLightingFeature, PanelLogoSpectrumBacklightFeature spectrumFeature)
     {
         _lenovoLightingFeature = lenovoLightingFeature ?? throw new ArgumentNullException(nameof(lenovoLightingFeature));
         _spectrumFeature = spectrumFeature ?? throw new ArgumentNullException(nameof(spectrumFeature));
+
+        _lazyAsyncFeature = new(GetFeatureLazyAsync);
     }
 
-    public async Task<bool> IsSupportedAsync() => await GetFeatureAsync().ConfigureAwait(false) != null;
+    public async Task<bool> IsSupportedAsync() => await _lazyAsyncFeature.Value.ConfigureAwait(false) != null;
 
     public async Task<PanelLogoBacklightState[]> GetAllStatesAsync()
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         return await feature.GetAllStatesAsync().ConfigureAwait(false);
     }
 
     public async Task<PanelLogoBacklightState> GetStateAsync()
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         return await feature.GetStateAsync().ConfigureAwait(false);
     }
 
     public async Task SetStateAsync(PanelLogoBacklightState state)
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         await feature.SetStateAsync(state).ConfigureAwait(false);
     }
 
-    private async Task<IFeature<PanelLogoBacklightState>?> GetFeatureAsync()
+    private async Task<IFeature<PanelLogoBacklightState>?> GetFeatureLazyAsync()
     {
-        if (_feature is not null)
-            return _feature;
-
         if (await _lenovoLightingFeature.IsSupportedAsync().ConfigureAwait(false))
-            return _feature = _lenovoLightingFeature;
+            return _lenovoLightingFeature;
 
         if (await _spectrumFeature.IsSupportedAsync().ConfigureAwait(false))
-            return _feature = _spectrumFeature;
+            return _spectrumFeature;
 
         return null;
     }
