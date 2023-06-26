37c74e3881a14289467c4af73d4084e268b4fb27
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-06-26 17:03:01 +0200

    Moved to lazy gets

--- LenovoLegionToolkit.Lib/Features/WhiteKeyboardBacklightFeature.cs
+++ LenovoLegionToolkit.Lib/Features/WhiteKeyboardBacklightFeature.cs
@@ -11,7 +11,7 @@ public class WhiteKeyboardBacklightFeature : IFeature<WhiteKeyboardBacklightStat
     private readonly SpectrumKeyboardBacklightController _spectrumController;
     private readonly RGBKeyboardBacklightController _rgbController;
 
-    private IFeature<WhiteKeyboardBacklightState>? _feature;
+    private readonly Lazy<Task<IFeature<WhiteKeyboardBacklightState>?>> _lazyAsyncFeature;
 
     public WhiteKeyboardBacklightFeature(WhiteKeyboardLenovoLightingBacklightFeature lenovoLightingFeature,
         WhiteKeyboardDriverBacklightFeature driverFeature,
@@ -23,41 +23,40 @@ public class WhiteKeyboardBacklightFeature : IFeature<WhiteKeyboardBacklightStat
         _driverFeature = driverFeature ?? throw new ArgumentNullException(nameof(driverFeature));
         _spectrumController = spectrumController ?? throw new ArgumentNullException(nameof(spectrumController));
         _rgbController = rgbController ?? throw new ArgumentNullException(nameof(rgbController));
+
+        _lazyAsyncFeature = new(GetFeatureLazyAsync);
     }
 
-    public async Task<bool> IsSupportedAsync() => await GetFeatureAsync().ConfigureAwait(false) != null;
+    public async Task<bool> IsSupportedAsync() => await _lazyAsyncFeature.Value.ConfigureAwait(false) != null;
 
     public async Task<WhiteKeyboardBacklightState[]> GetAllStatesAsync()
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         return await feature.GetAllStatesAsync().ConfigureAwait(false);
     }
 
     public async Task<WhiteKeyboardBacklightState> GetStateAsync()
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         return await feature.GetStateAsync().ConfigureAwait(false);
     }
 
     public async Task SetStateAsync(WhiteKeyboardBacklightState state)
     {
-        var feature = await GetFeatureAsync().ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
+        var feature = await _lazyAsyncFeature.Value.ConfigureAwait(false) ?? throw new InvalidOperationException("No supported feature found.");
         await feature.SetStateAsync(state).ConfigureAwait(false);
     }
 
-    private async Task<IFeature<WhiteKeyboardBacklightState>?> GetFeatureAsync()
+    private async Task<IFeature<WhiteKeyboardBacklightState>?> GetFeatureLazyAsync()
     {
-        if (_feature is not null)
-            return _feature;
-
         if (await _spectrumController.IsSupportedAsync().ConfigureAwait(false) || await _rgbController.IsSupportedAsync().ConfigureAwait(false))
             return null;
 
         if (await _lenovoLightingFeature.IsSupportedAsync().ConfigureAwait(false))
-            return _feature = _lenovoLightingFeature;
+            return _lenovoLightingFeature;
 
         if (await _driverFeature.IsSupportedAsync().ConfigureAwait(false))
-            return _feature = _driverFeature;
+            return _driverFeature;
 
         return null;
     }
