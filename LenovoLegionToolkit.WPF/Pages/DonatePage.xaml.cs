2cd36fafabdfca2a584270e2161c9027fa1558c3
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-07-03 17:16:52 +0200

    Remove stripe

--- LenovoLegionToolkit.WPF/Pages/DonatePage.xaml.cs
+++ LenovoLegionToolkit.WPF/Pages/DonatePage.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System.Globalization;
-using System.Windows;
+﻿using System.Windows;
 using LenovoLegionToolkit.WPF.Extensions;
-using LenovoLegionToolkit.WPF.Resources;
 
 namespace LenovoLegionToolkit.WPF.Pages;
 
@@ -10,17 +8,6 @@ public partial class DonatePage
     public DonatePage()
     {
         InitializeComponent();
-
-        if (Resource.Culture.Equals(new CultureInfo("zh-hans")))
-        {
-            _paypal.Visibility = Visibility.Collapsed;
-            _stripeChina.Visibility = Visibility.Visible;
-        }
-        else
-        {
-            _paypal.Visibility = Visibility.Visible;
-            _stripeChina.Visibility = Visibility.Collapsed;
-        }
     }
 
     private void PayPalDonateButton_Click(object sender, RoutedEventArgs e)
