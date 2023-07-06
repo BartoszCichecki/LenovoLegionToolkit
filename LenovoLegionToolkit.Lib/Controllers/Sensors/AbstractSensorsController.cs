a60ad6a6d9e9b329a7cdeaa81738000a7aff650f
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2023-07-07 01:36:56 +0200

    GetCPUFrequencyAsync

--- LenovoLegionToolkit.Lib/Controllers/Sensors/AbstractSensorsController.cs
+++ LenovoLegionToolkit.Lib/Controllers/Sensors/AbstractSensorsController.cs
@@ -154,17 +154,7 @@ public abstract class AbstractSensorsController : ISensorsController
         }
     }
 
-    private static Task<int> GetCpuMaxCoreClockAsync() => WMI.CallAsync("root\\WMI",
-        $"SELECT * FROM LENOVO_GAMEZONE_DATA",
-        "GetCPUFrequency",
-        new(),
-        pdc =>
-        {
-            var value = Convert.ToInt32(pdc["Data"].Value);
-            var low = value & 0xFFFF;
-            var high = value >> 16;
-            return Math.Max(low, high);
-        });
+    private static Task<int> GetCpuMaxCoreClockAsync() => WMI.LenovoGamezoneData.GetCPUFrequencyAsync();
 
     private GPUInfo GetGPUInfo()
     {
--- LenovoLegionToolkit.Lib/System/WMI.LenovoGameZoneData.cs
+++ LenovoLegionToolkit.Lib/System/WMI.LenovoGameZoneData.cs
@@ -17,5 +17,17 @@ public static partial class WMI
             $"SELECT * FROM LENOVO_GAMEZONE_DATA",
             "NotifyDGPUStatus",
             new() { { "Status", status ? 1 : 0 } });
+
+        public static Task<int> GetCPUFrequencyAsync() => WMI.CallAsync("root\\WMI",
+            $"SELECT * FROM LENOVO_GAMEZONE_DATA",
+            "GetCPUFrequency",
+            new(),
+            pdc =>
+            {
+                var value = Convert.ToInt32(pdc["Data"].Value);
+                var low = value & 0xFFFF;
+                var high = value >> 16;
+                return Math.Max(low, high);
+            });
     }
 }
