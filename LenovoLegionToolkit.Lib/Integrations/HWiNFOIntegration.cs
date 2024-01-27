b520cfab68c1875688033e295a9a0cd83d91f04b
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2024-01-27 19:19:05 +0100

    Added battery temperature to HWiNFO64 integration

--- LenovoLegionToolkit.Lib/Integrations/HWiNFOIntegration.cs
+++ LenovoLegionToolkit.Lib/Integrations/HWiNFOIntegration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using LenovoLegionToolkit.Lib.Controllers.Sensors;
@@ -13,8 +14,11 @@ public class HWiNFOIntegration
     private const string CUSTOM_SENSOR_HIVE = "HKEY_CURRENT_USER";
     private const string CUSTOM_SENSOR_PATH = @"Software\HWiNFO64\Sensors\Custom";
     private const string CUSTOM_SENSOR_GROUP_NAME = "Lenovo Legion Toolkit";
-    private const string CPU_SENSOR_NAME = "CPU Fan";
-    private const string GPU_SENSOR_NAME = "GPU Fan";
+    private const string SENSOR_TYPE_FAN = "Fan";
+    private const string SENSOR_TYPE_TEMP = "Temp";
+    private const string CPU_FAN_SENSOR_NAME = "CPU Fan";
+    private const string GPU_FAN_SENSOR_NAME = "GPU Fan";
+    private const string BATTERY_TEMP_SENSOR_NAME = "Battery Temperature";
 
     private readonly TimeSpan _refreshInterval = TimeSpan.FromSeconds(1);
 
@@ -64,38 +68,47 @@ public class HWiNFOIntegration
         {
             while (true)
             {
-                try
-                {
-                    token.ThrowIfCancellationRequested();
-                    await SetSensorValuesAsync().ConfigureAwait(false);
-                    await Task.Delay(_refreshInterval, token).ConfigureAwait(false);
-                }
-                catch (Exception ex)
-                {
-                    if (Log.Instance.IsTraceEnabled)
-                        Log.Instance.Trace($"Failed to set values.", ex);
-                }
+                token.ThrowIfCancellationRequested();
+                await SetSensorValuesAsync().ConfigureAwait(false);
+                await Task.Delay(_refreshInterval, token).ConfigureAwait(false);
+
             }
         }
         catch (OperationCanceledException) { }
+        catch (Exception ex)
+        {
+            if (Log.Instance.IsTraceEnabled)
+                Log.Instance.Trace($"Failed to set values.", ex);
+        }
     }
 
     private async Task SetSensorValuesAsync()
     {
         var (cpuFanSpeed, gpuFanSpeed) = await _sensorController.GetFanSpeedsAsync().ConfigureAwait(false);
+        var batteryTemp = Battery.GetBatteryInformation().BatteryTemperatureC;
 
-        SetValue(0, CPU_SENSOR_NAME, cpuFanSpeed);
-        SetValue(1, GPU_SENSOR_NAME, gpuFanSpeed);
+        SetValue(SENSOR_TYPE_FAN, 0, CPU_FAN_SENSOR_NAME, cpuFanSpeed);
+        SetValue(SENSOR_TYPE_FAN, 1, GPU_FAN_SENSOR_NAME, gpuFanSpeed);
+
+        if (batteryTemp.HasValue)
+        {
+            var nfi = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            SetValue(SENSOR_TYPE_TEMP, 0, BATTERY_TEMP_SENSOR_NAME, batteryTemp.Value.ToString(nfi));
+        }
+        else
+        {
+            SetValue(SENSOR_TYPE_TEMP, 0, BATTERY_TEMP_SENSOR_NAME, string.Empty);
+        }
     }
 
-    private static void SetValue(int index, string name, int value)
+    private static void SetValue<T>(string type, int index, string name, T value) where T : notnull
     {
         Registry.SetValue(CUSTOM_SENSOR_HIVE,
-            $@"{CUSTOM_SENSOR_PATH}\{CUSTOM_SENSOR_GROUP_NAME}\Fan{index}",
+            $@"{CUSTOM_SENSOR_PATH}\{CUSTOM_SENSOR_GROUP_NAME}\{type}{index}",
             "Name",
             name);
         Registry.SetValue(CUSTOM_SENSOR_HIVE,
-            $@"{CUSTOM_SENSOR_PATH}\{CUSTOM_SENSOR_GROUP_NAME}\Fan{index}",
+            $@"{CUSTOM_SENSOR_PATH}\{CUSTOM_SENSOR_GROUP_NAME}\{type}{index}",
             "Value",
             value);
     }
