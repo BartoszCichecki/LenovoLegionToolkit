6fb947049a3aa40553d39981f0fec2b96e7947de
Ace_Radom <sichenradomlyu@gmail.com>
2024-04-17 10:54:20 +0200

    speaker (un)mute control api

---
+++ LenovoLegionToolkit.Lib/System/Speaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using NAudio.CoreAudioApi;
+
+namespace LenovoLegionToolkit.Lib.System;
+
+public class Speaker
+{
+    public static void Mute() => ToggleMute(true);
+
+    public static void Unmute() => ToggleMute(false);
+
+    private static void ToggleMute(bool setMute)
+    {
+        var enumerator = new MMDeviceEnumerator();
+        MMDevice mMDevice = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active)
+                                      .ToArray()
+                                      .ToList()[0];
+        mMDevice.AudioEndpointVolume.Mute = setMute;
+    }
+}
