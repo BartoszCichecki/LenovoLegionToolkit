8e42afba083418e9801976df770bcc03df04e50c
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2024-06-04 22:02:20 +0200

    Refactor macro playback for readability.

--- LenovoLegionToolkit.Lib.Macro/Utils/MacroPlayer.cs
+++ LenovoLegionToolkit.Lib.Macro/Utils/MacroPlayer.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LenovoLegionToolkit.Lib.Extensions;
 using LenovoLegionToolkit.Lib.Utils;
 using Windows.Win32;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
@@ -54,73 +55,89 @@ internal class MacroPlayer
 
                     token.ThrowIfCancellationRequested();
 
-                    var input = ToInput(macroEvent, Screen.PrimaryScreen?.WorkingArea ?? Rectangle.Empty);
-                    PInvoke.SendInput(MemoryMarshal.CreateSpan(ref input, 1), Marshal.SizeOf<INPUT>());
+                    try
+                    {
+                        var input = ToInput(macroEvent, Screen.PrimaryScreen?.WorkingArea ?? Rectangle.Empty);
+                        var result = PInvoke.SendInput(MemoryMarshal.CreateSpan(ref input, 1), Marshal.SizeOf<INPUT>());
+                        if (result == 0)
+                            PInvokeExtensions.ThrowIfWin32Error($"Failed to send input. Return code was {result}.");
+                    }
+                    catch (Exception ex)
+                    {
+                        if (Log.Instance.IsTraceEnabled)
+                            Log.Instance.Trace($"Failed to send input for event {macroEvent}", ex);
+                    }
                 }
             }
         }, token);
     }
 
-    private static INPUT ToInput(MacroEvent macroEvent, Rectangle screenArea) => new()
+    private static INPUT ToInput(MacroEvent macroEvent, Rectangle screenArea) => macroEvent.Source switch
     {
-        type = macroEvent.Source switch
-        {
-            MacroSource.Mouse => INPUT_TYPE.INPUT_MOUSE,
-            MacroSource.Keyboard => INPUT_TYPE.INPUT_KEYBOARD,
-            MacroSource.Unknown => throw new ArgumentException(null, nameof(macroEvent)),
-            _ => throw new ArgumentOutOfRangeException(nameof(macroEvent))
-        },
+        MacroSource.Keyboard => ToKeyboardInput(macroEvent),
+        MacroSource.Mouse => ToMouseInput(macroEvent, screenArea),
+        MacroSource.Unknown => throw new ArgumentException(null, nameof(macroEvent)),
+        _ => throw new ArgumentOutOfRangeException(nameof(macroEvent))
+    };
+
+    private static INPUT ToKeyboardInput(MacroEvent macroEvent) => new()
+    {
+        type = INPUT_TYPE.INPUT_KEYBOARD,
         Anonymous = new INPUT._Anonymous_e__Union
         {
-            ki = macroEvent.Source is not MacroSource.Keyboard
-                ? default
-                : new KEYBDINPUT
+            ki = new KEYBDINPUT
+            {
+                wVk = (VIRTUAL_KEY)macroEvent.Key,
+                dwFlags = macroEvent.Direction switch
                 {
-                    wVk = (VIRTUAL_KEY)macroEvent.Key,
-                    dwFlags = macroEvent.Direction switch
-                    {
-                        MacroDirection.Up => KEYBD_EVENT_FLAGS.KEYEVENTF_KEYUP,
-                        _ => 0
-                    },
-                    dwExtraInfo = MAGIC_NUMBER
+                    MacroDirection.Up => KEYBD_EVENT_FLAGS.KEYEVENTF_KEYUP,
+                    _ => 0
                 },
-            mi = macroEvent.Source is not MacroSource.Mouse
-                ? default
-                : new MOUSEINPUT
+                dwExtraInfo = MAGIC_NUMBER
+            }
+        }
+    };
+
+    private static INPUT ToMouseInput(MacroEvent macroEvent, Rectangle screenArea) => new()
+    {
+        type = INPUT_TYPE.INPUT_MOUSE,
+        Anonymous = new INPUT._Anonymous_e__Union
+        {
+            mi = new MOUSEINPUT
+            {
+                dwFlags = (macroEvent.Direction, macroEvent.Key) switch
                 {
-                    dwFlags = (macroEvent.Direction, macroEvent.Key) switch
-                    {
-                        (MacroDirection.Up, 1) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTUP,
-                        (MacroDirection.Down, 1) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN,
-                        (MacroDirection.Up, 2) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP,
-                        (MacroDirection.Down, 2) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN,
-                        (MacroDirection.Up, 3) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEUP,
-                        (MacroDirection.Down, 3) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEDOWN,
-                        (MacroDirection.Up, > 0xFF) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_XUP,
-                        (MacroDirection.Down, > 0xFF) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_XDOWN,
-                        (MacroDirection.Wheel, _) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_WHEEL,
-                        (MacroDirection.Move, _) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE,
-                        _ => 0
-                    },
-                    mouseData = (macroEvent.Direction, macroEvent.Key) switch
-                    {
-                        (MacroDirection.Up, >= 0xFF) => macroEvent.Key >> 16,
-                        (MacroDirection.Down, >= 0xFF) => macroEvent.Key >> 16,
-                        (MacroDirection.Wheel, _) => macroEvent.Key,
-                        _ => 0
-                    },
-                    dx = macroEvent.Direction switch
-                    {
-                        MacroDirection.Move => (int)(65535.0f * (macroEvent.Point.X / (float)screenArea.Width) + 0.5f),
-                        _ => 0
-                    },
-                    dy = macroEvent.Direction switch
-                    {
-                        MacroDirection.Move => (int)(65535.0f * (macroEvent.Point.Y / (float)screenArea.Height) + 0.5f),
-                        _ => 0
-                    },
-                    dwExtraInfo = MAGIC_NUMBER
-                }
+                    (MacroDirection.Up, 1) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTUP,
+                    (MacroDirection.Down, 1) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_LEFTDOWN,
+                    (MacroDirection.Up, 2) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP,
+                    (MacroDirection.Down, 2) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN,
+                    (MacroDirection.Up, 3) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEUP,
+                    (MacroDirection.Down, 3) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MIDDLEDOWN,
+                    (MacroDirection.Up, > 0xFF) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_XUP,
+                    (MacroDirection.Down, > 0xFF) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_XDOWN,
+                    (MacroDirection.Wheel, _) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_WHEEL,
+                    (MacroDirection.Move, _) => MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE | MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE,
+                    _ => 0
+                },
+                mouseData = (macroEvent.Direction, macroEvent.Key) switch
+                {
+                    (MacroDirection.Up, >= 0xFF) => macroEvent.Key >> 16,
+                    (MacroDirection.Down, >= 0xFF) => macroEvent.Key >> 16,
+                    (MacroDirection.Wheel, _) => macroEvent.Key,
+                    _ => 0
+                },
+                dx = macroEvent.Direction switch
+                {
+                    MacroDirection.Move => (int)(65535.0f * (macroEvent.Point.X / (float)screenArea.Width) + 0.5f),
+                    _ => 0
+                },
+                dy = macroEvent.Direction switch
+                {
+                    MacroDirection.Move => (int)(65535.0f * (macroEvent.Point.Y / (float)screenArea.Height) + 0.5f),
+                    _ => 0
+                },
+                dwExtraInfo = MAGIC_NUMBER
+            }
         }
     };
 }
