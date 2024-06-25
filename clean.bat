66f1198ea5e7310172547f5a6f0f4a3df1a914ce
Ace_Radom <sichenradomlyu@gmail.com>
2024-06-25 13:42:30 +0200

    update cleanup script

--- clean.bat
+++ clean.bat
@@ -9,14 +9,17 @@ rmdir /s /q build_installer
 rmdir /s /q LenovoLegionToolkit.CLI\bin
 rmdir /s /q LenovoLegionToolkit.CLI\obj
 
+rmdir /s /q LenovoLegionToolkit.CLI.Lib\bin
+rmdir /s /q LenovoLegionToolkit.CLI.Lib\obj
+
 rmdir /s /q LenovoLegionToolkit.Lib\bin
 rmdir /s /q LenovoLegionToolkit.Lib\obj
 
+rmdir /s /q LenovoLegionToolkit.Lib.AoTOSD\x64
+rmdir /s /q LenovoLegionToolkit.Lib.AoTOSD\obj
+
 rmdir /s /q LenovoLegionToolkit.Lib.Automation\bin
 rmdir /s /q LenovoLegionToolkit.Lib.Automation\obj
-
-rmdir /s /q LenovoLegionToolkit.Lib.CLI\bin
-rmdir /s /q LenovoLegionToolkit.Lib.CLI\obj
 
 rmdir /s /q LenovoLegionToolkit.Lib.Macro\bin
 rmdir /s /q LenovoLegionToolkit.Lib.Macro\obj
