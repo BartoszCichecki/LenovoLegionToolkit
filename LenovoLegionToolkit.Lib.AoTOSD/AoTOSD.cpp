ca7869fd008cc21250d9f9e48825520a2c8b4181
Ace_Radom <sichenradomlyu@gmail.com>
2024-06-28 09:21:23 +0200

    reformat

--- LenovoLegionToolkit.Lib.AoTOSD/AoTOSD.cpp
+++ LenovoLegionToolkit.Lib.AoTOSD/AoTOSD.cpp
@@ -8,33 +8,33 @@
 namespace AoTOSD = LenovoLegionToolkit::Lib::AoTOSD;
 
 AoTOSD::Test::Test(::System::Drawing::Bitmap^ mbitmap) {
-	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
-	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
-	//Gdiplus::Image* image = new Gdiplus::Image(L"eject.png");
-	//Gdiplus::Bitmap* bitmap = new Gdiplus::Bitmap(image->GetWidth(), image->GetHeight(), PixelFormat32bppARGB);
+    //Gdiplus::Image* image = new Gdiplus::Image(L"eject.png");
+    //Gdiplus::Bitmap* bitmap = new Gdiplus::Bitmap(image->GetWidth(), image->GetHeight(), PixelFormat32bppARGB);
 
-	//Gdiplus::Graphics* graphics = Gdiplus::Graphics::FromImage(bitmap);
+    //Gdiplus::Graphics* graphics = Gdiplus::Graphics::FromImage(bitmap);
 
-	//graphics->DrawImage(image, 0, 0, image->GetWidth(), image->GetHeight());
+    //graphics->DrawImage(image, 0, 0, image->GetWidth(), image->GetHeight());
 
-	auto bitmap = Utils::BitmapConverter::Convert(mbitmap);
+    auto bitmap = Utils::BitmapConverter::Convert(mbitmap);
 
-	this->window = new Window::OSDWindow(L"LLT_AoTOSD_Test_1", L"LLT_AoTOSD_Test_1", 0);
-	this->window->SetBitmap(bitmap);
-	// this->window->SetHBitmap((HBITMAP)mbitmap->GetHbitmap(::System::Drawing::Color::FromArgb(0, 0, 0, 0)).ToPointer(), mbitmap->Width, mbitmap->Height);
-	this->window->SetVisibleDuration(1000);
-	this->window->SetPosition({ 128, 128 });
+    this->window = new Window::OSDWindow(L"LLT_AoTOSD_Test_1", L"LLT_AoTOSD_Test_1", 0);
+    this->window->SetBitmap(bitmap);
+    // this->window->SetHBitmap((HBITMAP)mbitmap->GetHbitmap(::System::Drawing::Color::FromArgb(0, 0, 0, 0)).ToPointer(), mbitmap->Width, mbitmap->Height);
+    this->window->SetVisibleDuration(1000);
+    this->window->SetPosition({ 128, 128 });
 
-	return;
+    return;
 }
 
 void AoTOSD::Test::Show() {
 
-	
+    
 
-	
-	this->window->Show();
-	return;
+    
+    this->window->Show();
+    return;
 }
