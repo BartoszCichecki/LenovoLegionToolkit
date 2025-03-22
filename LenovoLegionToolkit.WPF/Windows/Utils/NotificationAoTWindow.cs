b60616a301d94444ebfb5b5a857484ecb514127a
Ace_Radom <sichenradomlyu@gmail.com>
2025-03-22 23:38:41 +0100

    basic NotificationAoTWindow

---
+++ LenovoLegionToolkit.WPF/Windows/Utils/NotificationAoTWindow.cs
@@ -0,0 +1,102 @@
+﻿using LenovoLegionToolkit.Lib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace LenovoLegionToolkit.WPF.Windows.Utils;
+
+public class NotificationAoTWindow(ScreenInfo screenInfo, NotificationPosition position) : NativeLayeredWindow
+{
+    private readonly ScreenInfo _screenInfo = screenInfo;
+    private readonly NotificationPosition _position = position;
+
+    private Bitmap? _bitmap;
+
+    public void SetBitmap(Bitmap bitmap)
+    {
+        _bitmap = bitmap;
+        UpdatePosition();
+    }
+
+    public void Show(int closeAfter)
+    {
+        Show();
+        Task.Delay(closeAfter).ContinueWith(_ =>
+        {
+            Close();
+        }, TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    protected override void Paint(PaintEventArgs e)
+    {
+        if (_bitmap is not null)
+            e.Graphics.DrawImage(_bitmap, new Rectangle(0, 0, _bitmap.Width, _bitmap.Height));
+    }
+
+    private void UpdatePosition()
+    {
+        if (_bitmap is null)
+            return;
+
+        var multiplierX = _screenInfo.DpiX / 96d;
+        var multiplierY = _screenInfo.DpiY / 96d;
+        var wa = _screenInfo.WorkArea;
+        Rect workArea = new(wa.Left, wa.Top, wa.Width * multiplierX, wa.Height * multiplierY);
+
+        Size = new(_bitmap.Width, _bitmap.Height);
+
+        const int margin = 16;
+        var marginX = margin * multiplierX;
+        var marginY = margin * multiplierY;
+
+        double left = 0;
+        double top = 0;
+
+        switch (_position)
+        {
+            case NotificationPosition.BottomRight:
+                left = workArea.Right - Width - marginX;
+                top = workArea.Bottom - Height - marginY;
+                break;
+            case NotificationPosition.BottomCenter:
+                left = workArea.Left + (workArea.Width - Width) / 2;
+                top = workArea.Bottom - Height - marginY;
+                break;
+            case NotificationPosition.BottomLeft:
+                left = workArea.Left + marginX;
+                top = workArea.Bottom - Height - marginY;
+                break;
+            case NotificationPosition.CenterLeft:
+                left = workArea.Left + marginX;
+                top = workArea.Top + (workArea.Height - Height) / 2;
+                break;
+            case NotificationPosition.TopLeft:
+                left = workArea.Left + marginX;
+                top = workArea.Top + marginY;
+                break;
+            case NotificationPosition.TopCenter:
+                left = workArea.Left + (workArea.Width - Width) / 2;
+                top = workArea.Top + marginY;
+                break;
+            case NotificationPosition.TopRight:
+                left = workArea.Right - Width - marginX;
+                top = workArea.Top + marginY;
+                break;
+            case NotificationPosition.CenterRight:
+                left = workArea.Right - Width - marginX;
+                top = workArea.Top + (workArea.Height - Height) / 2;
+                break;
+            case NotificationPosition.Center:
+                left = workArea.Left + (workArea.Width - Width) / 2;
+                top = workArea.Top + (workArea.Height - Height) / 2;
+                break;
+        }
+
+        Position = new((int)left, (int)top);
+    }
+}
