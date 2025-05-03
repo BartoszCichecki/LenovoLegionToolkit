6071279b99a54c18fe55f18cdb65924955a1d30b
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2025-05-03 07:49:30 +0200

    Fix #1633

--- LenovoLegionToolkit.WPF/Utils/NotificationsManager.cs
+++ LenovoLegionToolkit.WPF/Utils/NotificationsManager.cs
@@ -261,7 +261,11 @@ public class NotificationsManager
         }
         else
         {
-            var nw = new NotificationWindow(symbol, overlaySymbol, symbolTransform, text, clickAction, ScreenHelper.PrimaryScreen, _settings.Store.NotificationPosition) { Owner = mainWindow };
+            var primaryScreen = ScreenHelper.PrimaryScreen;
+            if (!primaryScreen.HasValue)
+                return;
+
+            var nw = new NotificationWindow(symbol, overlaySymbol, symbolTransform, text, clickAction, primaryScreen.Value, _settings.Store.NotificationPosition) { Owner = mainWindow };
             nw.Show(_settings.Store.NotificationDuration switch
             {
                 NotificationDuration.Short => 500,
